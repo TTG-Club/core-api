21bb63319058f32a081d7f85fd8d965c68882f25
Vitalii Ungurean <vitalii@analytics.company>
2024-08-02 12:38:57 +0300

    Продолжение структуры проекта

---
+++ src/main/java/club/ttg/dnd5/controller/tools/generator/TreasuryController.java
@@ -0,0 +1,9 @@
+package club.ttg.dnd5.controller.tools.generator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TreasuryController {
+}
