23c0d1648d8c6b3446f2d4266b41d9109b7db48a
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-08-03 23:42:55 +0200

    - Обновление сущности - Добавление ДТО

---
+++ src/main/java/club/ttg/dnd5/dto/security/RegisterUserDTO.java
@@ -0,0 +1,15 @@
+package club.ttg.dnd5.dto.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class RegisterUserDTO {
+    private String email;
+    private String password;
+    private String fullName;
+
+}
