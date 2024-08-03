66ff080ed5e309ae831a5e7d6e4b07846db0753a
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-08-03 23:44:28 +0200

    - Добавление сервисов для поддержки Security

---
+++ src/main/java/club/ttg/dnd5/service/security/AuthenticationService.java
@@ -0,0 +1,41 @@
+package club.ttg.dnd5.service.security;
+
+import club.ttg.dnd5.dto.security.LoginUserDTO;
+import club.ttg.dnd5.dto.security.RegisterUserDTO;
+import club.ttg.dnd5.model.user.User;
+import club.ttg.dnd5.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public User signup(RegisterUserDTO input) {
+        User user = User.builder()
+                .name(input.getFullName())
+                .email(input.getEmail())
+                .password(passwordEncoder.encode(input.getPassword()))
+                .build();
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDTO input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
