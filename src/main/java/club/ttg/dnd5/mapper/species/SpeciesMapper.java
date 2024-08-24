03b4e3c333dd3319ff0b2ee35268053c439cbcf3
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-08-24 12:12:32 +0200

    - добавление секьюрити - изменение нейминга

--- src/main/java/club/ttg/dnd5/mapper/species/SpeciesMapper.java
+++ src/main/java/club/ttg/dnd5/mapper/species/SpeciesMapper.java
@@ -1,6 +1,6 @@
 package club.ttg.dnd5.mapper.species;
 
-import club.ttg.dnd5.dto.species.SpeciesDTO;
+import club.ttg.dnd5.dto.species.SpeciesResponse;
 import club.ttg.dnd5.model.character.Species;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -8,7 +8,7 @@ import org.mapstruct.factory.Mappers;
 @Mapper
 public interface SpeciesMapper {
     SpeciesMapper INSTANCE = Mappers.getMapper(SpeciesMapper.class);
-    SpeciesDTO speciesToSpeciesDTO(Species species);
-    Species speciesDTOToSpecies(SpeciesDTO speciesDTO);
+    SpeciesResponse speciesToSpeciesDTO(Species species);
+    Species speciesDTOToSpecies(SpeciesResponse speciesResponse);
 }
 
