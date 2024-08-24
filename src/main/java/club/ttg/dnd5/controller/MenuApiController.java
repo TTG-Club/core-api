a4157284e19252b1d44ce04efa847e806d40a3b9
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-08-24 03:26:55 +0200

    - добавил маппер - изменил в сущности ключ с айди на урл - подправил CRUD

--- src/main/java/club/ttg/dnd5/controller/MenuApiController.java
+++ src/main/java/club/ttg/dnd5/controller/MenuApiController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,38 +17,47 @@ import java.util.List;
 @RequestMapping("/api/v2/menu")
 public class MenuApiController {
     private final MenuService menuService;
+
     @Operation(summary = "Получение списка элементов меню")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<MenuApi> getAllMenus() {
-        return menuService.getAllMenus();
+        return menuService.findAll();
     }
 
-    @Operation(summary = "Получение элемента меню по ID")
-    @GetMapping("/{id}")
+    @Operation(summary = "Получение элемента меню по URL")
+    @GetMapping("/{url}")
     @ResponseStatus(HttpStatus.OK)
-    public MenuApi getMenuById(@PathVariable Long id) {
-        return menuService.getMenuById(id);
+    public ResponseEntity<MenuApi> getMenuByUrl(@PathVariable String url) {
+        return menuService.findByUrl(url)
+                .map(menuApi -> new ResponseEntity<>(menuApi, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @Operation(summary = "Создание нового элемента меню")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MenuApi createMenu(@RequestBody MenuApi menuApi) {
-        return menuService.createMenu(menuApi);
+        return menuService.save(menuApi);
     }
 
-    @Operation(summary = "Обновление элемента меню по ID")
-    @PutMapping("/{id}")
+    @Operation(summary = "Обновление элемента меню по URL")
+    @PutMapping("/{url}")
     @ResponseStatus(HttpStatus.OK)
-    public MenuApi updateMenu(@PathVariable Long id, @RequestBody MenuApi menuApi) {
-        return menuService.updateMenu(id, menuApi);
+    public ResponseEntity<MenuApi> updateMenu(@PathVariable String url, @RequestBody MenuApi menuApi) {
+        menuApi.setUrl(url);
+        try {
+            MenuApi updatedMenu = menuService.update(menuApi);
+            return new ResponseEntity<>(updatedMenu, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @Operation(summary = "Удаление элемента меню по ID")
-    @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление элемента меню по URL")
+    @DeleteMapping("/{url}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteMenu(@PathVariable Long id) {
-        menuService.deleteMenu(id);
+    public void deleteMenu(@PathVariable String url) {
+        menuService.deleteByUrl(url);
     }
 }
--- src/main/java/club/ttg/dnd5/repository/MenuRepository.java
+++ src/main/java/club/ttg/dnd5/repository/MenuRepository.java
@@ -4,7 +4,12 @@ import club.ttg.dnd5.model.engine.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface MenuRepository extends JpaRepository<Menu, Long> {
+public interface MenuRepository extends JpaRepository<Menu, String> {
+    Optional<Menu> findByUrl(String url);
+    boolean existsByUrl(String url);
+    void deleteByUrl(String url);
 }
 
