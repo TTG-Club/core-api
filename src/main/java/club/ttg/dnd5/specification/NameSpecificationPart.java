62ffad81a7e11b68b675a23f2809c2d20f0d89a1
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-08-30 23:25:14 +0200

    - добавление спецификации абстрактной и т.д

---
+++ src/main/java/club/ttg/dnd5/specification/NameSpecificationPart.java
@@ -0,0 +1,20 @@
+package club.ttg.dnd5.specification;
+
+import club.ttg.dnd5.dto.engine.SearchDto;
+import club.ttg.dnd5.dto.engine.SearchRequest;
+import org.springframework.data.jpa.domain.Specification;
+
+public abstract class NameSpecificationPart<T> implements SpecificationPart<T> {
+    @Override
+    public Specification<T> toSpecification(SearchRequest request) {
+        return (root, query, cb) -> {
+            if (request.getSearch() != null) {
+                SearchDto searchDto = request.getSearch();
+                if (searchDto.getValue() != null && !searchDto.getValue().isEmpty()) {
+                    return cb.like(cb.lower(root.get("name")), "%" + searchDto.getValue().toLowerCase() + "%");
+                }
+            }
+            return cb.conjunction(); // Пустая спецификация, если нет фильтрации по имени
+        };
+    }
+}
