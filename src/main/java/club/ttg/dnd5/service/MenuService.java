f91e0bb8826080546c7f424d98620db8d203a650
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-09-17 20:25:55 +0200

    - исправление замечаний, которые были найдены в PR

--- src/main/java/club/ttg/dnd5/service/MenuService.java
+++ src/main/java/club/ttg/dnd5/service/MenuService.java
@@ -1,7 +1,7 @@
 package club.ttg.dnd5.service;
 
 import club.ttg.dnd5.dto.engine.MenuResponse;
-import club.ttg.dnd5.exception.StorageException;
+import club.ttg.dnd5.exception.EntityNotFoundException;
 import club.ttg.dnd5.mapper.engine.MenuMapper;
 import club.ttg.dnd5.model.engine.Menu;
 import club.ttg.dnd5.repository.MenuRepository;
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -23,12 +21,13 @@ public class MenuService {
     public List<MenuResponse> findAll() {
         return menuRepository.findAll().stream()
                 .map(menuMapper::menuToMenuApi)
-                .collect(Collectors.toList());
+                .toList();
     }
 
-    public Optional<MenuResponse> findByUrl(String url) {
+    public MenuResponse findByUrl(String url) {
         return menuRepository.findByUrl(url)
-                .map(menuMapper::menuToMenuApi);
+                .map(menuMapper::menuToMenuApi)
+                .orElseThrow(EntityNotFoundException::new);
     }
 
     public MenuResponse save(MenuResponse menuResponse) {
@@ -37,13 +36,24 @@ public class MenuService {
         return menuMapper.menuToMenuApi(savedMenu);
     }
 
-    public MenuResponse update(MenuResponse menuResponse) {
-        if (menuRepository.existsByUrl(menuResponse.getUrl())) {
-            Menu menu = menuMapper.menuApiToMenu(menuResponse);
-            Menu updatedMenu = menuRepository.save(menu);
+    public MenuResponse update(String oldUrl, MenuResponse menuResponse) {
+        if (menuRepository.existsByUrl(oldUrl)) {
+            Menu existingMenu = menuRepository.findByUrl(oldUrl)
+                    .orElseThrow(() -> new EntityNotFoundException("Menu with URL " + oldUrl + " does not exist."));
+
+            updateExistingMenu(menuResponse, existingMenu);
+
+            if (menuResponse.getChildren() != null && !menuResponse.getChildren().isEmpty()) {
+                List<Menu> updatedChildren = menuResponse.getChildren().stream()
+                        .map(menuMapper::menuApiToMenu)
+                        .toList();
+                existingMenu.setChildren(updatedChildren);
+            }
+
+            Menu updatedMenu = menuRepository.save(existingMenu);
             return menuMapper.menuToMenuApi(updatedMenu);
         } else {
-            throw new StorageException("Menu with URL " + menuResponse.getUrl() + " does not exist.");
+            throw new EntityNotFoundException("Menu with URL " + oldUrl + " does not exist.");
         }
     }
 
@@ -51,7 +61,17 @@ public class MenuService {
         if (menuRepository.existsByUrl(url)) {
             menuRepository.deleteByUrl(url);
         } else {
-            throw new StorageException("Menu with URL " + url + " does not exist.");
+            throw new EntityNotFoundException("Menu with URL " + url + " does not exist.");
         }
+    }
+
+    private static void updateExistingMenu(MenuResponse menuResponse, Menu existingMenu) {
+        existingMenu.setName(menuResponse.getName());
+        existingMenu.setIcon(menuResponse.getIcon());
+        existingMenu.setUrl(menuResponse.getUrl());
+        existingMenu.setOnlyDev(menuResponse.getOnlyDev() != null ? menuResponse.getOnlyDev() : existingMenu.isOnlyDev());
+        existingMenu.setOrder(menuResponse.getOrder());
+        existingMenu.setOnIndex(menuResponse.getOnIndex() != null ? menuResponse.getOnIndex() : existingMenu.isOnIndex());
+        existingMenu.setIndexOrder(menuResponse.getIndexOrder());
     }
 }
