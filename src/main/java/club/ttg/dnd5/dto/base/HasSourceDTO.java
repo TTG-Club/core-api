9e591820e73aab40a31addbe4f0d730299230cd8
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-09-27 16:36:47 +0200

    - Добавлены методы в конвертер, для поддержки Source конвертеров. Также интерфейс, для более Generic вида, чтобы можно было передавать много что

---
+++ src/main/java/club/ttg/dnd5/dto/base/HasSourceDTO.java
@@ -0,0 +1,8 @@
+package club.ttg.dnd5.dto.base;
+
+public interface HasSourceDTO {
+    String getSource();
+    Short getPage();
+    void setPage(Short Page);
+    void setSource(String source);
+}
