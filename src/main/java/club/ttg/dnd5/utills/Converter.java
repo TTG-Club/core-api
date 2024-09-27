0c4fba752d7b85a493b419e3635dc61ace8f50e2
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-09-27 16:05:46 +0200

    - Изменена логика в конвертере, теперь возращается объект, который был изменен - Также подправлен неймниг

--- src/main/java/club/ttg/dnd5/utills/Converter.java
+++ src/main/java/club/ttg/dnd5/utills/Converter.java
@@ -6,28 +6,30 @@ import club.ttg.dnd5.dto.species.CreaturePropertiesDTO;
 import club.ttg.dnd5.model.base.CreatureProperties;
 import club.ttg.dnd5.model.base.NamedEntity;
 
-//TODO change maybe from void to Objects
 public class Converter {
     private Converter() {}
-    //D from DTO, E from Entity
-    public static  <D extends BaseDTO, E extends NamedEntity> void fillEntityNameFromBaseDTO(D dto, E entity) {
+
+    // D from DTO, E from Entity
+    public static <D extends BaseDTO, E extends NamedEntity> E mapBaseDTOToEntityName(D dto, E entity) {
         entity.setUrl(dto.getUrl());
         entity.setName(dto.getNameBasedDTO().getName());
         entity.setEnglish(dto.getNameBasedDTO().getEnglish());
         entity.setAlternative(dto.getNameBasedDTO().getAlternative());
         entity.setDescription(dto.getNameBasedDTO().getDescription());
+        return entity;
     }
 
-    public static  <D extends BaseDTO, E extends NamedEntity> void fillDTOFromEntity(D dto, E entity) {
+    public static <D extends BaseDTO, E extends NamedEntity> D mapEntityToBaseDTO(D dto, E entity) {
         dto.setUrl(entity.getUrl());
         dto.setNameBasedDTO(new NameBasedDTO());
         dto.getNameBasedDTO().setName(entity.getName());
         dto.getNameBasedDTO().setEnglish(entity.getEnglish());
         dto.getNameBasedDTO().setAlternative(entity.getAlternative());
         dto.getNameBasedDTO().setDescription(entity.getDescription());
+        return dto;
     }
 
-    public static <D extends CreaturePropertiesDTO, E extends CreatureProperties> void fillEntityCreaturePropertiesFromDTO(D dto, E entity) {
+    public static <D extends CreaturePropertiesDTO, E extends CreatureProperties> E mapCreaturePropertiesDTOToEntity(D dto, E entity) {
         entity.setSize(dto.getSize());
         entity.setType(dto.getType());
         entity.setSpeed(dto.getSpeed());
@@ -35,9 +37,10 @@ public class Converter {
         entity.setClimb(dto.getClimb());
         entity.setSwim(dto.getSwim());
         entity.setDarkVision(dto.getDarkVision());
+        return entity;
     }
 
-    public static <D extends CreaturePropertiesDTO, E extends CreatureProperties> void fillDTOCreaturePropertiesFromEntity(D dto, E entity) {
+    public static <D extends CreaturePropertiesDTO, E extends CreatureProperties> D mapEntityToCreaturePropertiesDTO(D dto, E entity) {
         dto.setSize(entity.getSize());
         dto.setType(entity.getType());
         dto.setSpeed(entity.getSpeed());
@@ -45,5 +48,6 @@ public class Converter {
         dto.setClimb(entity.getClimb());
         dto.setSwim(entity.getSwim());
         dto.setDarkVision(entity.getDarkVision());
+        return dto;
     }
 }
