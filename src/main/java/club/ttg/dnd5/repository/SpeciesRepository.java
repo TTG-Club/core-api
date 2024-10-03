f29c084455ed597c3f5d5c37842afa86fe428bf3
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-10-03 22:04:43 +0200

    - Убрал лишний TODO в `BaseDTO` - Подправил в `Species` компановку коллекции, чтобы лучше было оптимизировано - Добавил в репу, новый метод - В Сервис & API, были добавлены два новых метода, как и просил Никита

--- src/main/java/club/ttg/dnd5/repository/SpeciesRepository.java
+++ src/main/java/club/ttg/dnd5/repository/SpeciesRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SpeciesRepository extends JpaRepository<Species, String>, JpaSpecificationExecutor<Species> {
+    List<Species> findByParent(Species parent);
 }
