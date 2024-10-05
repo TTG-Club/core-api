b2479555e55136b26e3cf8696fd42d57be47990f
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-10-05 15:34:18 +0200

    - Добавлена поддержка tags.

--- src/main/java/club/ttg/dnd5/dto/species/SpeciesFeatureResponse.java
+++ src/main/java/club/ttg/dnd5/dto/species/SpeciesFeatureResponse.java
@@ -1,11 +1,15 @@
 package club.ttg.dnd5.dto.species;
 
 import club.ttg.dnd5.dto.base.BaseDTO;
+import club.ttg.dnd5.model.base.HasTags;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
-public class SpeciesFeatureResponse extends BaseDTO {
+public class SpeciesFeatureResponse extends BaseDTO implements HasTags {
     private String description;
+    private Map<String, String> tags;
 }
--- src/main/java/club/ttg/dnd5/model/species/SpeciesFeature.java
+++ src/main/java/club/ttg/dnd5/model/species/SpeciesFeature.java
@@ -1,13 +1,16 @@
 package club.ttg.dnd5.model.species;
 
 import club.ttg.dnd5.model.base.FeatureBase;
+import club.ttg.dnd5.model.base.HasTags;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 @Table(name = "species_features",
@@ -17,9 +20,15 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SpeciesFeature extends FeatureBase {
+public class SpeciesFeature extends FeatureBase implements HasTags {
     @ElementCollection
     @CollectionTable(name = "species_feature_entries", joinColumns = @JoinColumn(name = "species_feature_id"))
     @Column(name = "entry")
     private List<String> entries;
+    // Храним карту tags как отдельную таблицу с ключами и значениями
+    @ElementCollection
+    @CollectionTable(name = "entity_tags", joinColumns = @JoinColumn(name = "entity_url"))
+    @MapKeyColumn(name = "tag_key")
+    @Column(name = "tag_value")
+    private Map<String, String> tags = new HashMap<>();
 }
