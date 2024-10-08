35cfc062d46fb8138ad4a7179c7b0b33814e6f54
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-10-08 20:28:45 +0200

    - добавление тегов - добавление спецификации - Задел под будущий рефактор

--- src/main/java/club/ttg/dnd5/dto/base/BaseDTO.java
+++ src/main/java/club/ttg/dnd5/dto/base/BaseDTO.java
@@ -3,8 +3,6 @@ package club.ttg.dnd5.dto.base;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Map;
-
 /*
  В @Override можно будет добавлять кастомную логику, если понадобится
  */
@@ -15,7 +13,4 @@ public abstract class BaseDTO {
     private String imageUrl;
     private NameBasedDTO nameBasedDTO;
     private SourceResponse source;
-    //подумать как сохранять в бдшке, тэги
-    //Сделать поиск по тегу
-    private Map<String, String> tags;
 }
