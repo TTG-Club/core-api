0486cc5081f78b2b8fcb4fa0be5b52a064a89102
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-10-20 11:36:55 +0200

    Переделаны feature, корректная запись и поддержка теперь в базу данных и возвращение на фронт

--- src/main/java/club/ttg/dnd5/utills/species/SpeciesFeatureConverter.java
+++ src/main/java/club/ttg/dnd5/utills/species/SpeciesFeatureConverter.java
@@ -1,13 +1,11 @@
 package club.ttg.dnd5.utills.species;
 
-import club.ttg.dnd5.dto.EntryDto;
 import club.ttg.dnd5.dto.species.SpeciesFeatureResponse;
 import club.ttg.dnd5.model.species.Species;
 import club.ttg.dnd5.model.species.SpeciesFeature;
 import club.ttg.dnd5.utills.Converter;
 
 import java.util.Collection;
-import java.util.Collections;
 
 public class SpeciesFeatureConverter {
     private SpeciesFeatureConverter() {
@@ -19,15 +17,16 @@ public class SpeciesFeatureConverter {
         Converter.mapBaseDTOToEntityName(response, speciesFeature);
         Converter.mapDTOSourceToEntitySource(response, speciesFeature);
         speciesFeature.setTags(response.getTags());
+        speciesFeature.setFeatureDescription(response.getDescription());
         return speciesFeature;
     }
 
     public static SpeciesFeatureResponse toDTOFeature(SpeciesFeature feature) {
         SpeciesFeatureResponse dto = new SpeciesFeatureResponse();
-        dto.setUrl(feature.getUrl());
-        EntryDto entries = new EntryDto();
-        entries.setName(feature.getName());
-        entries.setEntries(Collections.singletonList(feature.getEntries()));
+        Converter.mapEntityToBaseDTO(dto, feature);
+        Converter.mapEntitySourceToDTOSource(dto, feature);
+        dto.setTags(feature.getTags());
+        dto.setDescription(feature.getFeatureDescription());
         return dto;
     }
 
@@ -36,6 +35,11 @@ public class SpeciesFeatureConverter {
                 .map(SpeciesFeatureConverter::toEntityFeature)
                 .toList();
         species.setFeatures(features);
+    }
 
+    public static Collection<SpeciesFeatureResponse> convertEntityFeatureIntoDTOFeature(Collection<SpeciesFeature> entityFeatures) {
+        return entityFeatures.stream()
+                .map(SpeciesFeatureConverter::toDTOFeature)
+                .toList();
     }
 }
