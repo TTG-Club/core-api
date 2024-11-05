da4faab395db2e1bd873978223707f58b95482d7
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-11-05 19:48:10 +0100

    - исправил на биФункцию, потому что тут есть маленький выиграш оптимизации. Функция не создается, и просто всегда используется & банально, в декларативном стиле используется.
    - В будущем, я планирую сделать ещё 1 слой абстракции в Конвертере, чтобы туда можно было передавать уже функции

--- src/main/java/club/ttg/dnd5/utills/Converter.java
+++ src/main/java/club/ttg/dnd5/utills/Converter.java
@@ -13,11 +13,13 @@ import club.ttg.dnd5.model.book.Source;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.function.BiFunction;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Converter {
-    // D from DTO, E from Entity
 
-    public static <D extends BaseDTO, E extends NamedEntity> E mapBaseDtoToEntityName(D dto, E entity) {
+    // Function to map base DTO to Entity Name
+    public static final BiFunction<BaseDTO, NamedEntity, NamedEntity> MAP_BASE_DTO_TO_ENTITY_NAME = (dto, entity) -> {
         entity.setUrl(dto.getUrl());
         entity.setImageUrl(dto.getImageUrl());
         entity.setName(dto.getNameBasedDTO().getName());
@@ -25,9 +27,10 @@ public class Converter {
         entity.setAlternative(dto.getNameBasedDTO().getAlternative());
         entity.setDescription(dto.getNameBasedDTO().getDescription());
         return entity;
-    }
+    };
 
-    public static <D extends BaseDTO, E extends NamedEntity> D mapEntityToBaseDto(D dto, E entity) {
+    // Function to map Entity Name to Base DTO
+    public static final BiFunction<BaseDTO, NamedEntity, BaseDTO> MAP_ENTITY_TO_BASE_DTO = (dto, entity) -> {
         dto.setUrl(entity.getUrl());
         dto.setImageUrl(entity.getImageUrl());
         dto.setNameBasedDTO(new NameBasedDTO());
@@ -36,9 +39,10 @@ public class Converter {
         dto.getNameBasedDTO().setAlternative(entity.getAlternative());
         dto.getNameBasedDTO().setDescription(entity.getDescription());
         return dto;
-    }
+    };
 
-    public static <D extends CreaturePropertiesDTO, E extends CreatureProperties> E mapCreaturePropertiesDtoToEntity(D dto, E entity) {
+    // Function to map Creature Properties DTO to Entity
+    public static final BiFunction<CreaturePropertiesDTO, CreatureProperties, CreatureProperties> MAP_CREATURE_PROPERTIES_DTO_TO_ENTITY = (dto, entity) -> {
         entity.setSize(dto.getSize());
         entity.setType(dto.getType());
         entity.setSpeed(dto.getSpeed());
@@ -47,9 +51,10 @@ public class Converter {
         entity.setSwim(dto.getSwim());
         entity.setDarkVision(dto.getDarkVision());
         return entity;
-    }
+    };
 
-    public static <D extends CreaturePropertiesDTO, E extends CreatureProperties> D mapEntityToCreaturePropertiesDto(D dto, E entity) {
+    // Function to map Creature Properties Entity to DTO
+    public static final BiFunction<CreaturePropertiesDTO, CreatureProperties, CreaturePropertiesDTO> MAP_ENTITY_TO_CREATURE_PROPERTIES_DTO = (dto, entity) -> {
         dto.setSize(entity.getSize());
         dto.setType(entity.getType());
         dto.setSpeed(entity.getSpeed());
@@ -58,11 +63,10 @@ public class Converter {
         dto.setSwim(entity.getSwim());
         dto.setDarkVision(entity.getDarkVision());
         return dto;
-    }
+    };
 
-    //D - dto, E - entity, Id - id (acronym), R - repository
-    public static <D extends HasSourceDTO, E extends HasSourceEntity>
-    E mapDtoSourceToEntitySource(D dto, E entity) {
+    // Function to map DTO Source to Entity Source
+    public static final BiFunction<HasSourceDTO, HasSourceEntity, HasSourceEntity> MAP_DTO_SOURCE_TO_ENTITY_SOURCE = (dto, entity) -> {
         String sourceAcronym = dto.getSource();
         Source source = entity.getSource();
         if (source == null) {
@@ -74,19 +78,21 @@ public class Converter {
         source.setPage(dto.getPage());
         entity.setSource(source);
         return entity;
-    }
+    };
 
-    public static <D extends HasSourceDTO, E extends HasSourceEntity> D mapEntitySourceToDtoSource(D dto, E entity) {
+    // Function to map Entity Source to DTO Source
+    public static final BiFunction<HasSourceDTO, HasSourceEntity, HasSourceDTO> MAP_ENTITY_SOURCE_TO_DTO_SOURCE = (dto, entity) -> {
         if (entity.getSource() != null) {
             dto.setSource(entity.getSource().getSourceAcronym());
             dto.setPage(entity.getSource().getPage());
         }
         return dto;
-    }
+    };
 
-    public static <D extends BaseDTO & DetailableDTO, E extends NamedEntity> D mapEntityToBaseDtoWithHideDetails(D dto, E entity) {
-        mapEntityToBaseDto(dto, entity);
-        dto.hideDetails();
+    // Function to map Entity to Base DTO with hidden details
+    public static final BiFunction<BaseDTO, NamedEntity, BaseDTO> MAP_ENTITY_TO_BASE_DTO_WITH_HIDE_DETAILS = (dto, entity) -> {
+        MAP_ENTITY_TO_BASE_DTO.apply(dto, entity);
+        ((DetailableDTO) dto).hideDetails();
         return dto;
-    }
+    };
 }
