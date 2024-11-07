951035bbf40ebde50aeb19086adcc02b48f81840
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-11-07 19:31:19 +0100

    - Добавил поддержку isHiddenEntity, чтобы отображать лишь актуальные сущности. Также добавил фильтры под эти условия

--- src/main/java/club/ttg/dnd5/model/base/NamedEntity.java
+++ src/main/java/club/ttg/dnd5/model/base/NamedEntity.java
@@ -21,4 +21,12 @@ public abstract class NamedEntity extends TimestampedEntity {
     @Column(columnDefinition = "TEXT")
     private String description;
     private String imageUrl;
+    /**
+     * Indicates whether this entity should be hidden from the frontend.
+     * <p>
+     * If {@code true}, this entity is considered outdated or irrelevant, and it will not be included
+     * in responses sent to the frontend. If {@code false}, the entity will be visible to the frontend.
+     * </p>
+     */
+    private boolean isHiddenEntity = false;
 }
