9afffdbbea57d529c288b526c2ef214091fc6e63
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-11-09 12:10:19 +0100

    - обновил конфигурацию Swagger

--- src/main/java/club/ttg/dnd5/config/OpenApiConfig.java
+++ src/main/java/club/ttg/dnd5/config/OpenApiConfig.java
@@ -2,9 +2,11 @@ package club.ttg.dnd5.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,16 +14,30 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().
-                        addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes
-                        ("Bearer Authentication", createAPIKeyScheme()))
-                .info(new Info().title("TTG REST API"));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(createApiInfo())
+                .addServersItem(new Server().url("/").description("Default Server URL"));
     }
 
     private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
                 .bearerFormat("JWT")
                 .scheme("bearer");
+    }
+
+    private Info createApiInfo() {
+        return new Info()
+                .title("TTG REST API")
+                .version("2.0")
+                .description("This is the API documentation for the TTG application.")
+                .contact(new Contact()
+                        .name("Support Team")
+                        .email("support@ttg.club")
+                        .url("https://ttg.club"));
     }
 }
