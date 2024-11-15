13904b7630d7a5a2e51a44306f404906d96e311b
Vitalii Ungurean <vitalii@analytics.company>
2024-11-15 17:26:55 +0200

    Исправление контроллера видов [species]

--- src/main/java/club/ttg/dnd5/dto/base/BaseDTO.java
+++ src/main/java/club/ttg/dnd5/dto/base/BaseDTO.java
@@ -1,5 +1,6 @@
 package club.ttg.dnd5.dto.base;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import lombok.Setter;
 public abstract class BaseDTO {
     private String url;
     private String imageUrl;
+    @JsonProperty("name")
     private NameBasedDTO nameBasedDTO;
     private SourceResponse sourceDTO = new SourceResponse();
 }
