40e5f2fa686bf664fcf8b73674827196b7e74981
Vitalii Ungurean <vitalii@analytics.company>
2024-11-15 17:35:04 +0200

    Удалил поля из json которые null

--- src/main/java/club/ttg/dnd5/dto/base/SourceResponse.java
+++ src/main/java/club/ttg/dnd5/dto/base/SourceResponse.java
@@ -1,11 +1,13 @@
 package club.ttg.dnd5.dto.base;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Setter
 @AllArgsConstructor
