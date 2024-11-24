a62f03b5a50faf8b88fb243eac49decf58fa8b11
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-11-24 14:53:10 +0100

    - added dependency for s3 - upd application.properties - add S3Config

---
+++ src/main/java/club/ttg/dnd5/config/S3Config.java
@@ -0,0 +1,29 @@
+package club.ttg.dnd5.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    public AmazonS3 s3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+
+}
