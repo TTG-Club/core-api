64cde5eb49b95cef56182e24505d6e97599419da
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-12-10 20:56:27 +0100

    - добавление тега в фичюры

--- src/main/java/club/ttg/dnd5/model/character/ClassFeature.java
+++ src/main/java/club/ttg/dnd5/model/character/ClassFeature.java
@@ -3,12 +3,15 @@ package club.ttg.dnd5.model.character;
 import club.ttg.dnd5.model.base.FeatureBase;
 import club.ttg.dnd5.model.base.HasSourceEntity;
 import club.ttg.dnd5.model.base.HasTagEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
+import club.ttg.dnd5.model.base.Tag;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Getter
 @Setter
@@ -20,4 +23,12 @@ import lombok.Setter;
 )
 public class ClassFeature extends FeatureBase implements HasSourceEntity, HasTagEntity {
     private short level;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "class_feature_tags", // Name of the join table
+            joinColumns = @JoinColumn(name = "class_feature_id"), // Foreign key for ClassFeature
+            inverseJoinColumns = @JoinColumn(name = "tag_id") // Foreign key for Tag
+    )
+    private Set<Tag> tags = new HashSet<>();
 }
