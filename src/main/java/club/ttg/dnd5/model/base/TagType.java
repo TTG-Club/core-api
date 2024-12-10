8a10e9165a20268540fb17339494b01950452d66
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-12-10 20:46:22 +0100

    - мелкие исправление на стороне сущности, добавление @Builder - исправление логики в сервисы, добавление под лейер ДТО - исправление контроллеров - удаление старого контроллера

---
+++ src/main/java/club/ttg/dnd5/model/base/TagType.java
@@ -0,0 +1,6 @@
+package club.ttg.dnd5.model.base;
+
+public enum TagType {
+    TAG_BOOK,
+    TAG_FEATURE,
+}
--- src/main/java/club/ttg/dnd5/service/book/BookService.java
+++ src/main/java/club/ttg/dnd5/service/book/BookService.java
@@ -1,20 +1,25 @@
 package club.ttg.dnd5.service.book;
 
-import club.ttg.dnd5.dto.create.CreateSourceDTO;
+import club.ttg.dnd5.dto.base.NameBasedDTO;
+import club.ttg.dnd5.dto.base.TranslationDTO;
+import club.ttg.dnd5.dto.book.SourceBookDTO;
+import club.ttg.dnd5.exception.ApiException;
 import club.ttg.dnd5.exception.EntityNotFoundException;
 import club.ttg.dnd5.model.base.Tag;
+import club.ttg.dnd5.model.base.TagType;
+import club.ttg.dnd5.model.base.Translation;
 import club.ttg.dnd5.model.book.Book;
 import club.ttg.dnd5.model.book.TypeBook;
 import club.ttg.dnd5.repository.TagRepository;
 import club.ttg.dnd5.repository.book.BookRepository;
 import club.ttg.dnd5.service.TagService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,81 +30,112 @@ public class BookService {
     private final TagService tagService;
 
     // Создание новой книги
-    public Book createBook(CreateSourceDTO createSourceDTO) {
-        Book book = convertingCreateSourceToEntity(createSourceDTO);
-
-        // Преобразуем строки тегов в объекты Tag
-        Set<Tag> tagSet = book.getTags().stream()
-                .map(Tag::getName)
-                .map(tagName -> tagRepository.findByNameIgnoreCase(tagName)
-                        .orElseGet(() -> tagService.createTag(tagName))) // Создаем тег, если его нет
-                .collect(Collectors.toSet());
-
-        // Устанавливаем теги для книги
-        book.setTags(tagSet);
-
-        // Сохраняем книгу
-        return bookRepository.save(book);
+    @Transactional
+    public SourceBookDTO createBook(SourceBookDTO sourceBookDTO) {
+        Book book = convertingCreateSourceToEntity(sourceBookDTO);
+        Book savedBook = bookRepository.save(book);
+        return convertingEntityToSourceDTO(savedBook);
     }
 
     // Получение книги по её sourceAcronym
-    public Optional<Book> getBookBySourceAcronym(String sourceAcronym) {
-        return bookRepository.findBySourceAcronym(sourceAcronym);
+    public Optional<SourceBookDTO> getBookBySourceAcronym(String sourceAcronym) {
+        return bookRepository.findBySourceAcronym(sourceAcronym)
+                .map(this::convertingEntityToSourceDTO);
     }
 
     // Поиск книги по типу
-    public List<Book> getBooksByType(String type) {
+    public List<SourceBookDTO> getBooksByType(String type) {
         TypeBook bookType = TypeBook.valueOf(type.toUpperCase());
-        return bookRepository.findByType(bookType);
+        return bookRepository.findByType(bookType).stream()
+                .map(this::convertingEntityToSourceDTO)
+                .collect(Collectors.toList());
     }
 
     // Получение всех типов книг
     public List<String> getAllBookTypes() {
         return Arrays.stream(TypeBook.values())
                 .map(TypeBook::getName)
-                .collect(Collectors.toList());
-    }
-
-    // Добавление тегов к книге
-    public Book addTagsToBook(String sourceAcronym, Set<String> tags) {
-        Book book = bookRepository.findBySourceAcronym(sourceAcronym)
-                .orElseThrow(() -> new EntityNotFoundException("Book not found"));
-
-        Set<Tag> tagSet = tags.stream()
-                .map(tagName -> tagRepository.findByNameIgnoreCase(tagName)
-                        .orElseGet(() -> tagService.createTag(tagName))) // Создаем тег, если его нет
-                .collect(Collectors.toSet());
-
-        book.getTags().addAll(tagSet);
-        return bookRepository.save(book);
+                .toList();
     }
 
     // Получение всех книг с определённым тегом
-    public List<Book> getBooksByTag(String tagName) {
+    public List<SourceBookDTO> getBooksByTag(String tagName) {
         Tag tag = tagRepository.findByNameIgnoreCase(tagName)
                 .orElseThrow(() -> new EntityNotFoundException("Tag not found"));
 
-        return bookRepository.findByTags(tag);
+        return bookRepository.findByTags(tag).stream()
+                .map(this::convertingEntityToSourceDTO)
+                .collect(Collectors.toList());
     }
 
-    private static Book convertingCreateSourceToEntity(CreateSourceDTO createSourceDTO) {
-        // Извлекаем данные из DTO
-        String sourceAcronym = createSourceDTO.getName().getName();  // Получаем имя как источник
-        String name = createSourceDTO.getName().getName();
-        String altName = createSourceDTO.getName().getAlternative();
-        String englishName = createSourceDTO.getName().getEnglish();
-        String description = createSourceDTO.getDescription();
-        String type = createSourceDTO.getType();
-        Integer year = createSourceDTO.getYear() != null ? Integer.valueOf(createSourceDTO.getYear()) : null;
+    private Book convertingCreateSourceToEntity(SourceBookDTO sourceBookDTO) {
+        NameBasedDTO name = sourceBookDTO.getName();
+        if (StringUtils.isBlank(name.getShortName())) {
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Акроним у книги должен быть, это является ID, в бдшке");
+        }
+        return Book.builder()
+                .year(sourceBookDTO.getYear())
+                .sourceAcronym(name.getShortName())
+                .name(name.getName())
+                .englishName(name.getEnglish())
+                .authors(sourceBookDTO.getAuthor())
+                .image(sourceBookDTO.getImage())
+                .description(sourceBookDTO.getDescription())
+                .tags(generatingTags(sourceBookDTO.getTags()))
+                .type(TypeBook.valueOf(sourceBookDTO.getType()))
+                .translation(convertingTranslation(sourceBookDTO.getTranslation()))
+                .build();
+    }
 
-        // Создаем новую книгу
-        Book book = new Book(sourceAcronym);
-        book.setName(name);
-        book.setAltName(altName);
-        book.setEnglishName(englishName);
-        book.setDescription(description);
-        book.setType(TypeBook.valueOf(type.toUpperCase()));  // Предполагаем, что в DTO передается имя типа в строчном регистре
-        book.setYear(year);
-        return book;
+    private Set<Tag> generatingTags(Set<String> tags) {
+        return tags
+                .stream()
+                .map(tagName ->
+                        new Tag(tagName, TagType.TAG_BOOK))
+                .collect(Collectors.toSet());
+    }
+
+    private Translation convertingTranslation(TranslationDTO translationDTO) {
+        return Translation.
+                builder()
+                .year(translationDTO.getYear())
+                .authors(translationDTO.getAuthor())
+                .build();
+    }
+
+    private SourceBookDTO convertingEntityToSourceDTO(Book book) {
+        if (book == null || StringUtils.isBlank(book.getSourceAcronym())) {
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Сущность книги невалидная");
+        }
+
+        return SourceBookDTO.builder()
+                .year(book.getYear())
+                .name(NameBasedDTO.builder()
+                        .shortName(book.getSourceAcronym())
+                        .name(book.getName())
+                        .english(book.getEnglishName())
+                        .build())
+                .author(new HashSet<>(book.getAuthors()))
+                .image(book.getImage())
+                .description(book.getDescription())
+                .tags(book.getTags().stream()
+                        .map(Tag::getName)
+                        .collect(Collectors.toSet()))
+                .type(book.getType().name())
+                .translation(convertingTranslationToDTO(book.getTranslation()))
+                .build();
+    }
+
+    private TranslationDTO convertingTranslationToDTO(Translation translation) {
+        if (translation == null) {
+            return null;
+        }
+
+        return TranslationDTO.builder()
+                .year(translation.getYear())
+                .author(new HashSet<>(translation.getAuthors()))
+                .build();
     }
 }
