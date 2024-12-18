401fb07f6e5e1ad323ddb26a476e6f0e2a1d1b9c
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2024-12-18 14:39:05 +0100

    - исправление json - добавил аннотацию с JsonInclude

--- src/main/java/club/ttg/dnd5/dto/book/SourceBookDTO.java
+++ src/main/java/club/ttg/dnd5/dto/book/SourceBookDTO.java
@@ -3,6 +3,7 @@ package club.ttg.dnd5.dto.book;
 import club.ttg.dnd5.dto.base.HasTagDTO;
 import club.ttg.dnd5.dto.base.NameBasedDTO;
 import club.ttg.dnd5.dto.base.TranslationDTO;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,6 +24,7 @@ public class SourceBookDTO implements HasTagDTO {
     private String type;
     private String image;
     private Set<String> author = new HashSet<>();
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private TranslationDTO translation;
     private Set<String> tags = new HashSet<>();
 }
