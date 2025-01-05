c99583cd271fa61d9dff4fd05bf586a21cec0476
svifty7 <svifty7@gmail.com>
2025-01-06 02:50:22 +0300

    feat: загрузка файлов в с3

--- src/main/java/club/ttg/dnd5/controller/ExceptionController.java
+++ src/main/java/club/ttg/dnd5/controller/ExceptionController.java
@@ -4,18 +4,28 @@ import club.ttg.dnd5.dto.ResponseDto;
 import club.ttg.dnd5.exception.ApiException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.io.IOException;
 
 @Log4j2
 @ControllerAdvice
+@RequiredArgsConstructor
 public class ExceptionController {
+
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String MAX_FILE_SIZE;
+
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ResponseDto> handleAuthenticationException(ApiException ex, HttpServletRequest request, HttpServletResponse response) {
         log.error(ExceptionUtils.getStackTrace(ex));
@@ -39,7 +49,14 @@ public class ExceptionController {
         return convertToResponseEntity(HttpStatus.BAD_REQUEST, message);
     }
 
-    @ExceptionHandler({Exception.class})
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ResponseDto> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex, HttpServletRequest request, HttpServletResponse response) {
+        String message = String.format("Максимальный размер загружаемых файлов – %s", MAX_FILE_SIZE);
+
+        return convertToResponseEntity(HttpStatus.BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler({IOException.class, Exception.class})
     public ResponseEntity<ResponseDto> handleOtherExceptions(Exception ex, HttpServletRequest request, HttpServletResponse response) {
         log.error(ExceptionUtils.getStackTrace(ex));
 
