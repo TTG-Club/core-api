8267a8382326cd220d14fc0d90d03186a8254f88
Vitalii Ungurean <vitalii@analytics.company>
2025-02-26 22:06:54 +0200

    Рефакторинг всего кода, с переносом в доменную структуру

--- src/main/java/club/ttg/dnd5/domain/beastiary/rest/dto/BeastShortResponse.java
+++ src/main/java/club/ttg/dnd5/domain/beastiary/rest/dto/BeastShortResponse.java
@@ -3,9 +3,13 @@ package club.ttg.dnd5.domain.beastiary.rest.dto;
 import club.ttg.dnd5.domain.common.rest.dto.BaseDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class BeastShortResponse extends BaseDto {
     @Schema(name = "CR", description = "Уровень опасности")
     private String challengeRailing;
+    @Schema(description = "Тип (или типы) существ")
+    private String type;
 }
