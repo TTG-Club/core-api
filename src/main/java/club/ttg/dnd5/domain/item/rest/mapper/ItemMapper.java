696947cf17687d5c8d69edcde50212c7a1e9d156
Vitalii Ungurean <vitalii@analytics.company>
2025-03-01 22:30:15 +0200

    Рефакторинг всего кода, с переносом в доменную структуру

--- src/main/java/club/ttg/dnd5/domain/item/rest/mapper/ItemMapper.java
+++ src/main/java/club/ttg/dnd5/domain/item/rest/mapper/ItemMapper.java
@@ -7,14 +7,24 @@ import club.ttg.dnd5.domain.item.rest.dto.ItemDetailResponse;
 import club.ttg.dnd5.domain.item.rest.dto.ItemRequest;
 import club.ttg.dnd5.domain.item.rest.dto.ItemShortResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
+    @Mapping(source = "name", target = "name.name")
+    @Mapping(source = "english", target = "name.english")
     ItemDetailResponse toDetailDto(final Item item);
+    @Mapping(source = "name", target = "name.name")
+    @Mapping(source = "english", target = "name.english")
     ItemDetailResponse toDetailDto(final Armor item);
+    @Mapping(source = "name", target = "name.name")
+    @Mapping(source = "english", target = "name.english")
     ItemDetailResponse toDetailDto(final Weapon item);
-
+    @Mapping(source = "name", target = "name.name")
+    @Mapping(source = "english", target = "name.english")
     ItemShortResponse toShortDto(Item item);
 
+    @Mapping(source = "name.name", target = "name")
+    @Mapping(source = "name.english", target = "english")
     Item toEntity(ItemRequest itemDto);
 }
