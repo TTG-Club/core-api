cfe66aea850fc65b71571911a9d9758d5b779886
Vitalii Ungurean <vitalii@analytics.company>
2025-03-01 19:04:36 +0200

    Рефакторинг всего кода, с переносом в доменную структуру

--- src/main/java/club/ttg/dnd5/domain/species/rest/mapper/SpeciesMapper.java
+++ src/main/java/club/ttg/dnd5/domain/species/rest/mapper/SpeciesMapper.java
@@ -1,17 +1,12 @@
 package club.ttg.dnd5.domain.species.rest.mapper;
 
-import club.ttg.dnd5.domain.beastiary.model.BeastSize;
 import club.ttg.dnd5.domain.species.model.Species;
-import club.ttg.dnd5.domain.species.model.SpeciesSize;
 import club.ttg.dnd5.domain.species.rest.dto.SpeciesDetailResponse;
 import club.ttg.dnd5.domain.species.rest.dto.SpeciesShortResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", uses = SpeciesFeatureMapper.class)
 public interface SpeciesMapper {
@@ -21,7 +16,7 @@ public interface SpeciesMapper {
     @Mapping(source = "english", target = "name.english")
     @Mapping(source = "updatedAt", target = "updatedAt")
     @Mapping(source = "darkVision", target = "properties.darkVision")
-    @Mapping(source = "sizes", target = "properties.size", qualifiedByName = "sizesToString")
+    @Mapping(source = "size.text", target = "properties.size")
     SpeciesDetailResponse toDetailDto(Species species);
 
     @Mapping(source = "name", target = "name.name")
@@ -35,9 +30,4 @@ public interface SpeciesMapper {
     @Mapping(source = "name.name", target = "name")
     @Mapping(source = "name.english", target = "english")
     Species toEntity(SpeciesRequest request);
-
-    @Named("sizesToString")
-    default String sizesToString(Collection<SpeciesSize> sizes) {
-        return sizes.stream().map(SpeciesSize::getSizeString).collect(Collectors.joining(" и "));
-    }
 }
