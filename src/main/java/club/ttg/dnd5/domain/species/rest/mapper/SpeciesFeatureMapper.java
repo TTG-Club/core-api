008b189dee876d9bae88ef3729b5fa82f76c2c56
Vitalii Ungurean <vitalii@analytics.company>
2025-03-02 14:51:48 +0200

    Быстрофикс

--- src/main/java/club/ttg/dnd5/domain/species/rest/mapper/SpeciesFeatureMapper.java
+++ src/main/java/club/ttg/dnd5/domain/species/rest/mapper/SpeciesFeatureMapper.java
@@ -3,12 +3,19 @@ package club.ttg.dnd5.domain.species.rest.mapper;
 import club.ttg.dnd5.domain.species.model.SpeciesFeature;
 import club.ttg.dnd5.domain.species.rest.dto.FeatureRequest;
 import club.ttg.dnd5.domain.species.rest.dto.SpeciesFeatureResponse;
+import club.ttg.dnd5.util.SlugifyUtil;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface SpeciesFeatureMapper {
     SpeciesFeatureResponse toDto(SpeciesFeature speciesFeature);
-
+    @Mapping(target = "url", source = "name", qualifiedByName = "generateUrl")
     SpeciesFeature toEntity(FeatureRequest request);
+
+    @Named("generateUrl")
+    default String generateUrl(String name) {
+        return SlugifyUtil.getSlug(name);
+    }
 }
