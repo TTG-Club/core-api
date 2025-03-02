22f084c7ab9bedfd0de0d86954a85cdcabdaf3e3
Vitalii Ungurean <vitalii@analytics.company>
2025-03-02 12:40:06 +0200

    Добавление инструментов

--- src/main/java/club/ttg/dnd5/domain/item/model/Tool.java
+++ src/main/java/club/ttg/dnd5/domain/item/model/Tool.java
@@ -1,4 +1,22 @@
 package club.ttg.dnd5.domain.item.model;
 
-public class Tool {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Инструмент
+ */
+@Getter
+@Setter
+@Entity
+@DiscriminatorValue("TOOL")
+public class Tool extends Item {
+
+    public String ability;
+
+    public String uses;
+
+    public String creation;
 }
