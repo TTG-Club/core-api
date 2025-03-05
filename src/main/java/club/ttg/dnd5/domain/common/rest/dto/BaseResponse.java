00b0e11e86d91be167e0a02b53f391488895cbbf
yrlvdplsh <yrlvdplsh@yandex.ru>
2025-03-05 17:49:14 +0300

    Получение заклинаний

--- src/main/java/club/ttg/dnd5/domain/common/rest/dto/BaseResponse.java
+++ src/main/java/club/ttg/dnd5/domain/common/rest/dto/BaseResponse.java
@@ -1,7 +1,7 @@
 package club.ttg.dnd5.domain.common.rest.dto;
 
 import club.ttg.dnd5.domain.common.model.TagType;
-import club.ttg.dnd5.dto.base.source.SourceResponse;
+import club.ttg.dnd5.dto.base.SourceResponse;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
--- src/main/java/club/ttg/dnd5/domain/common/rest/dto/ShortResponse.java
+++ src/main/java/club/ttg/dnd5/domain/common/rest/dto/ShortResponse.java
@@ -1,10 +1,11 @@
 package club.ttg.dnd5.domain.common.rest.dto;
 
-import club.ttg.dnd5.dto.base.source.SourceResponse;
+import club.ttg.dnd5.dto.base.SourceResponse;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
