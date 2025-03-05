ea35687b06d7a95240c97b65b0289b3380f4e996
Vitalii Ungurean <vitalii@analytics.company>
2025-03-05 20:44:52 +0200

    Исправление видов под новую форму

--- src/main/java/club/ttg/dnd5/domain/species/rest/dto/FeatureRequest.java
+++ src/main/java/club/ttg/dnd5/domain/species/rest/dto/FeatureRequest.java
@@ -1,5 +1,6 @@
 package club.ttg.dnd5.domain.species.rest.dto;
 
+import club.ttg.dnd5.domain.common.rest.dto.NameRequest;
 import club.ttg.dnd5.dto.base.SourceResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FeatureRequest {
-    private String name;
+    private NameRequest name;
     private String description;
     @Schema(description = "источник", requiredMode = Schema.RequiredMode.REQUIRED)
     private SourceResponse source = new SourceResponse();
