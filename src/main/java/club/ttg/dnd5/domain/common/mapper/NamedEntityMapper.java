cc52359d82d6bb95b0f8d1acb31d487f51679e63
yrlvdplsh <yrlvdplsh@yandex.ru>
2025-03-05 17:49:14 +0300

    Получение заклинаний

--- src/main/java/club/ttg/dnd5/domain/common/mapper/NamedEntityMapper.java
+++
@@ -1,9 +0,0 @@
-package club.ttg.dnd5.domain.common.mapper;
-
-import club.ttg.dnd5.domain.common.rest.dto.NameResponse;
-import club.ttg.dnd5.domain.spell.model.Spell;
-import org.mapstruct.Mapper;
-
-@Mapper
-public interface NamedEntityMapper {
-}
