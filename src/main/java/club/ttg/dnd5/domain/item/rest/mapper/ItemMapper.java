ee3b2e5ca3d064e879aeb094d8ad10d7f7838d73
Vitalii Ungurean <vitalii@analytics.company>
2025-03-06 13:29:31 +0200

    маппер предметов

--- src/main/java/club/ttg/dnd5/domain/item/rest/mapper/ItemMapper.java
+++ src/main/java/club/ttg/dnd5/domain/item/rest/mapper/ItemMapper.java
@@ -4,37 +4,35 @@ import club.ttg.dnd5.domain.item.model.*;
 import club.ttg.dnd5.domain.item.rest.dto.ItemDetailResponse;
 import club.ttg.dnd5.domain.item.rest.dto.ItemRequest;
 import club.ttg.dnd5.domain.item.rest.dto.ItemShortResponse;
+import club.ttg.dnd5.dto.base.mapping.BaseMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
-    @Mapping(source = "name", target = "name.name")
-    @Mapping(source = "english", target = "name.english")
+    @BaseMapping.BaseShortResponseNameMapping
     @Mapping(target = "type", constant = "ITEM")
     ItemDetailResponse toDetailDto(final Item item);
-    @Mapping(source = "name", target = "name.name")
-    @Mapping(source = "english", target = "name.english")
+
+    @BaseMapping.BaseShortResponseNameMapping
     @Mapping(target = "type", constant = "ARMOR")
     ItemDetailResponse toDetailDto(final Armor armor);
-    @Mapping(source = "name", target = "name.name")
-    @Mapping(source = "english", target = "name.english")
+
+    @BaseMapping.BaseShortResponseNameMapping
     @Mapping(target = "type", constant = "WEAPON")
     ItemDetailResponse toDetailDto(final Weapon weapon);
-    @Mapping(source = "name", target = "name.name")
-    @Mapping(source = "english", target = "name.english")
+
+    @BaseMapping.BaseShortResponseNameMapping
     @Mapping(target = "type", constant = "SHIP")
     ItemDetailResponse toDetailDto(final Ship ship);
-    @Mapping(source = "name", target = "name.name")
-    @Mapping(source = "english", target = "name.english")
+
+    @BaseMapping.BaseShortResponseNameMapping
     @Mapping(target = "type", constant = "MOUNT")
     ItemDetailResponse toDetailDto(final Mount mount);
 
-    @Mapping(source = "name", target = "name.name")
-    @Mapping(source = "english", target = "name.english")
+    @BaseMapping.BaseShortResponseNameMapping
     ItemShortResponse toShortDto(Item item);
 
-    @Mapping(source = "name.name", target = "name")
-    @Mapping(source = "name.english", target = "english")
-    Item toEntity(ItemRequest itemDto);
+    @BaseMapping.BaseEntityNameMapping
+    Item toEntity(ItemRequest request);
 }
