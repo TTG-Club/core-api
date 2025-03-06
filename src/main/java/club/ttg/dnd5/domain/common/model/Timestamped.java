e78fe9b6fe7fcc24706bae72eec182263c0a21ff
Vitaliy Unguryan <gm.magistr@gmail.com>
2025-03-06 22:42:59 +0200

    Сохранение имени пользователя при добавлении сущности (#79)
    Co-authored-by: Vitalii Ungurean <vitalii@analytics.company>

--- src/main/java/club/ttg/dnd5/domain/common/model/Timestamped.java
+++ src/main/java/club/ttg/dnd5/domain/common/model/Timestamped.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SourceType;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
 
 import java.time.Instant;
 
@@ -20,5 +21,7 @@ public abstract class Timestamped {
     @Column(name = "updated_at")
     @UpdateTimestamp(source = SourceType.DB)
     private Instant updatedAt;
+    @CreatedBy
+    @Column(updatable = false)
     private String username;
 }
