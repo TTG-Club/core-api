4d774a80c50e3b0029d5ea211601ce3d2608e5ab
Vitalii Ungurean <vitalii@analytics.company>
2025-03-06 10:20:36 +0200

    фикс времени накладывания в справочнике, добавление ритуала

--- src/main/java/club/ttg/dnd5/domain/spell/model/enums/CastingUnit.java
+++ src/main/java/club/ttg/dnd5/domain/spell/model/enums/CastingUnit.java
@@ -14,6 +14,7 @@ public enum CastingUnit implements NumberFormatted<CastingUnit> {
     ACTION("действие"),
     ROUND("ход"),
     MINUTE("минута"),
+    RITUAL("ритуал"),
     HOUR("час");
 
     private final String name;
