3a217133fc61f32806648b1bca3b901d4ed458a7
Denis <denis.petrov56245@gmail.com>
2025-03-20 19:33:20 +0100

    creating a glossary table creating a service (base-not finished

---
+++ src/main/java/club/ttg/dnd5/domain/glossary/repository/GlossaryRepository.java
@@ -0,0 +1,19 @@
+package club.ttg.dnd5.domain.glossary.repository;
+
+import club.ttg.dnd5.domain.glossary.model.Glossary;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface GlossaryRepository extends JpaRepository<Glossary, String> {
+    @Query(value = """
+            select g from Glossary g
+            where g.name ilike concat('%', :searchLine, '%')
+               or g.english ilike concat('%', :searchLine, '%')
+               or g.alternative ilike concat('%', :searchLine, '%')
+            """
+    )
+    List<Glossary> findBySearchLine(String searchLine, Sort sort);
+}
