29727b52b883180c4a9cf4bb10e3cffc149048bc
Vitalii Ungurean <vitalii@analytics.company>
2025-03-24 14:19:51 +0200

    Поиск по всем разделам

---
+++ src/main/java/club/ttg/dnd5/domain/common/service/FullSearchServiceImpl.java
@@ -0,0 +1,10 @@
+package club.ttg.dnd5.domain.common.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FullSearchServiceImpl {
+
+}
