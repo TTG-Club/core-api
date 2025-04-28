e11c8aaf4b1484ac6b533c5cb124a8d3664f6e8b
yrlvdplsh <yrlvdplsh@yandex.ru>
2025-04-28 11:44:20 +0300

    Ручка с количеством материалов на сайте

---
+++ src/main/java/club/ttg/dnd5/domain/cache/rest/controller/CacheController.java
@@ -0,0 +1,26 @@
+package club.ttg.dnd5.domain.cache.rest.controller;
+
+
+import club.ttg.dnd5.domain.cache.service.CacheService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Tag(name = "Кэщ", description = "API для управление кэшем")
+@RequestMapping("/api/v2/cache/")
+@RestController
+public class CacheController {
+    private final CacheService cacheService;
+
+    @Operation(summary = "Очистка кэша")
+    @GetMapping("/evict-all")
+    @Secured("ADMIN")
+    public void evictAll() {
+        cacheService.evictCaches();
+    }
+}
