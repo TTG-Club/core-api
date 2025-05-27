312d4c294f07e0a164040776c20aea5dcb825897
Vitalii Ungurean <vitalii@analytics.company>
2025-05-27 10:37:03 +0300

    [Бестиарий] изменение справочника характеристик

--- src/main/java/club/ttg/dnd5/domain/common/rest/dto/select/KeySelectDto.java
+++ src/main/java/club/ttg/dnd5/domain/common/rest/dto/select/KeySelectDto.java
@@ -1,11 +1,11 @@
 package club.ttg.dnd5.domain.common.rest.dto.select;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Setter;
+import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
-@Setter
+@Getter
 public class KeySelectDto extends BaseSelectOptionDto {
     @Schema(description = "ключ к сущности")
     private String key;
