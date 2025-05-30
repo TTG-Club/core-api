a92e7f08f2772c7149a483c7a166317d34c7ad17
Vitalii Ungurean <vitalii@analytics.company>
2025-05-30 13:55:12 +0300

    [Рейтинг] добавление total в ответ

--- src/main/java/club/ttg/dnd5/domain/common/rest/RatingMapper.java
+++ src/main/java/club/ttg/dnd5/domain/common/rest/RatingMapper.java
@@ -1,10 +1,22 @@
 package club.ttg.dnd5.domain.common.rest;
 
 import club.ttg.dnd5.domain.common.model.Rating;
+import club.ttg.dnd5.domain.common.repository.RatingStats;
 import club.ttg.dnd5.domain.common.rest.dto.rating.RatingRequest;
+import club.ttg.dnd5.domain.common.rest.dto.rating.RatingResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface RatingMapper {
     Rating toRating(RatingRequest request, String username);
+
+    @Mapping(source = "value", target = "value", qualifiedByName = "round")
+    RatingResponse toResponse(RatingStats ratingStats);
+
+    @Named("round")
+    default Double round(Double value) {
+        return value == null ? 0 : Math.round(value * 10.0) / 10.0;
+    }
 }
