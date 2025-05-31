32279abada2cd297a80b568edf63592936f63dc8
Vitalii Ungurean <vitalii@analytics.company>
2025-05-31 14:55:53 +0300

    [Бестиарий] переделка инициативы

---
+++ src/main/java/club/ttg/dnd5/domain/beastiary/model/CreatureInitiative.java
@@ -0,0 +1,11 @@
+package club.ttg.dnd5.domain.beastiary.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreatureInitiative {
+    private byte value;
+    private byte multiplier;
+}
