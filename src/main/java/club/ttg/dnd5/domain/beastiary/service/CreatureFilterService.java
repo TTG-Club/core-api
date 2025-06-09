05cc0bce6087e0c45b8dbc0f10bea22c1360962d
Vitalii Ungurean <vitalii@analytics.company>
2025-06-09 14:14:18 +0300

    :fix/bestiary исправление фильтра по УО

--- src/main/java/club/ttg/dnd5/domain/beastiary/service/CreatureFilterService.java
+++ src/main/java/club/ttg/dnd5/domain/beastiary/service/CreatureFilterService.java
@@ -1,21 +1,22 @@
 package club.ttg.dnd5.domain.beastiary.service;
 
+import club.ttg.dnd5.domain.beastiary.model.filter.CreatureSavedFilter;
+import club.ttg.dnd5.domain.beastiary.repository.CreatureSavedFilterRepository;
 import club.ttg.dnd5.domain.beastiary.rest.dto.filter.CrFilterGroup;
 import club.ttg.dnd5.domain.filter.model.FilterInfo;
 import club.ttg.dnd5.domain.filter.service.AbstractSavedFilterService;
-import club.ttg.dnd5.domain.spell.model.filter.SpellSavedFilter;
-import club.ttg.dnd5.domain.spell.repository.SpellSavedFilterRepository;
 import club.ttg.dnd5.domain.user.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CreatureFilterService extends AbstractSavedFilterService<SpellSavedFilter> {
+public class CreatureFilterService extends AbstractSavedFilterService<CreatureSavedFilter> {
     private static final String FILTER_VERSION = "1.0";
 
-    public CreatureFilterService(SpellSavedFilterRepository spellSavedFilterRepository, UserService userService) {
-        super(spellSavedFilterRepository, userService);
+    public CreatureFilterService(CreatureSavedFilterRepository creatureSavedFilterRepository,
+                                 UserService userService) {
+        super(creatureSavedFilterRepository, userService);
     }
 
     @Override
