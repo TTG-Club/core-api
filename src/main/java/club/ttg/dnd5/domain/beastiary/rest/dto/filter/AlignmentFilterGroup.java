dcde419aa898be0b3697a3290861f70d49105a71
Denis <denis.petrov56245@gmail.com>
2025-07-09 16:32:06 +0200

    несколько фильтров для бестиария (будут обновляться)

---
+++ src/main/java/club/ttg/dnd5/domain/beastiary/rest/dto/filter/AlignmentFilterGroup.java
@@ -0,0 +1,57 @@
+package club.ttg.dnd5.domain.beastiary.rest.dto.filter;
+
+import club.ttg.dnd5.domain.beastiary.model.enumus.AlignmentFilter;
+import club.ttg.dnd5.domain.spell.rest.dto.filter.SpellSchoolFilterGroup;
+import club.ttg.dnd5.dto.base.filters.AbstractFilterGroup;
+import club.ttg.dnd5.dto.base.filters.AbstractFilterItem;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.StringPath;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class AlignmentFilterGroup extends AbstractFilterGroup<AlignmentFilter, AlignmentFilterGroup.AlignmentFilterItem> {
+
+    private static final StringPath PATH = Expressions.stringPath("alignment");
+
+    public AlignmentFilterGroup(List<AlignmentFilterItem> filters) {
+        super(filters);
+    }
+
+    @Override
+    public BooleanExpression getQuery() {
+        if (isSingular()) {
+            return TRUE_EXPRESSION;
+        }
+        Set<AlignmentFilter> positiveValues = getPositive();
+        BooleanExpression result = CollectionUtils.isEmpty(positiveValues) ? TRUE_EXPRESSION : PATH.in(positiveValues.stream().map(AlignmentFilter::toString).collect(Collectors.toSet()));
+        Set<AlignmentFilter> negativeValues = getNegative();
+        return result.and(CollectionUtils.isEmpty(negativeValues) ? (TRUE_EXPRESSION) : PATH.notIn(negativeValues.stream().map(AlignmentFilter::toString).collect(Collectors.toSet())));
+
+    }
+
+    public String getName() {
+        return "Мировозрение";
+    }
+
+    public static AlignmentFilterGroup getDefault() {
+        return new AlignmentFilterGroup(
+                Arrays.stream(AlignmentFilter.values())
+                        .map(AlignmentFilterGroup.AlignmentFilterItem::new)
+                        .collect(Collectors.toList()));
+    }
+
+    public static class AlignmentFilterItem extends AbstractFilterItem<AlignmentFilter> {
+        public AlignmentFilterItem(AlignmentFilter alignment) {
+            super(alignment.getName(), alignment, null);
+        }
+    }
+}
