3595cb3a70d4343a856d148dd74cd0b32d970edc
Denis <denis.petrov56245@gmail.com>
2025-07-11 14:57:55 +0200

    чувства умения

---
+++ src/main/java/club/ttg/dnd5/domain/beastiary/model/enumus/CreatureSenses.java
@@ -0,0 +1,26 @@
+package club.ttg.dnd5.domain.beastiary.model.enumus;
+
+import club.ttg.dnd5.exception.ApiException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum CreatureSenses {
+    TRUESIGHT("Истинное зрение"),
+    BLINDSIGHT("Слепое зрение"),
+    DARKVISION("Темное зрение"),
+    TREMORSENSE("Чувство вибраций");
+
+    private final String name;
+
+    public static CreatureSenses parse(String name) {
+        for (CreatureSenses senses : values()) {
+            if (senses.name.equalsIgnoreCase(name)) {
+                return senses;
+            }
+        }
+        throw new ApiException(HttpStatus.NOT_FOUND, "Неправильное чувство");
+    }
+}
