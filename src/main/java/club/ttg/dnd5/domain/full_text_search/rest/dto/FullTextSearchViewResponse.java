97560256f0bb07842b48a996883b14e04d766695
yrlvdplsh <92540081+yrlvdplsh@users.noreply.github.com>
2025-08-05 00:06:17 +0300

    Feature/full text search (#145)
    * Поиск по всем разделам
    
    * Поиск по всем разделам
    
    * Поиск по всем разделам
    
    * Поиск по всем разделам

--- src/main/java/club/ttg/dnd5/domain/full_text_search/rest/dto/FullTextSearchViewResponse.java
+++ src/main/java/club/ttg/dnd5/domain/full_text_search/rest/dto/FullTextSearchViewResponse.java
@@ -1,19 +1,22 @@
 package club.ttg.dnd5.domain.full_text_search.rest.dto;
 
-import club.ttg.dnd5.domain.common.rest.dto.NameResponse;
-import club.ttg.dnd5.domain.full_text_search.model.FullTextSearchViewType;
-import club.ttg.dnd5.dto.base.SourceResponse;
-import lombok.*;
 
-@AllArgsConstructor
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Builder
 public class FullTextSearchViewResponse {
 
-    private String url;
-    private NameResponse name = new NameResponse();
-    private FullTextSearchViewType type;
-    private SourceResponse source;
+    List<FullTextSearchViewDto> result;
+
+    Long total;
 }
