ebb17f47358c98e7d2dfa74ac5203c9b0799fa93
Nikita Charushin <45195424+svifty7@users.noreply.github.com>
2025-08-06 14:47:02 +0300

    Обновление глобального поиска (#146)
    * feat: обновил ручку глобального поиска
    
    * feat: обновил ручку глобального поиска
    
    * fix: изменил подсчет элементов поиска
    
    * fix: правки ревью
    
    * fix: правки ревью
    
    * fix: правки ревью
    
    * feat: добавил бестиарий
    
    * feat: добавил changeset для liquibase на создание вьюхи глобального поиска
    
    * feat: добавил в changeset магические предметы

--- src/main/java/club/ttg/dnd5/domain/full_text_search/service/FullTextSearchViewService.java
+++ src/main/java/club/ttg/dnd5/domain/full_text_search/service/FullTextSearchViewService.java
@@ -1,15 +1,18 @@
 package club.ttg.dnd5.domain.full_text_search.service;
 
 import club.ttg.dnd5.domain.common.rest.dto.NameResponse;
+import club.ttg.dnd5.domain.full_text_search.model.FullTextSearchView;
+import club.ttg.dnd5.domain.full_text_search.model.FullTextSearchViewType;
 import club.ttg.dnd5.domain.full_text_search.repository.FullTextSearchViewRepository;
 import club.ttg.dnd5.domain.full_text_search.rest.dto.FullTextSearchViewDto;
 import club.ttg.dnd5.domain.full_text_search.rest.dto.FullTextSearchViewResponse;
 import club.ttg.dnd5.dto.base.SourceResponse;
 import club.ttg.dnd5.util.SwitchLayoutUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -18,37 +21,76 @@ import java.util.stream.Collectors;
 public class FullTextSearchViewService {
     private final FullTextSearchViewRepository fullTextSearchViewRepository;
 
+    @Value("${global-search.limit-per-group:5}")
+    private int maxItemsPerGroup;
+
     //TODO доделать маппинг
     public FullTextSearchViewResponse findBySearchLine(String searchLine) {
         return Optional.ofNullable(searchLine)
                 .filter(Predicate.not(String::isBlank))
                 .map(line -> fullTextSearchViewRepository.findBySearchLine(line, SwitchLayoutUtils.switchLayout(line)))
-                .map(results ->
-                        FullTextSearchViewResponse.builder()
-                                .result(results.stream().map(ftsv ->
-                                        FullTextSearchViewDto.builder()
-                                                .url(ftsv.getUrl())
-                                                .name(NameResponse.builder()
-                                                        .name(ftsv.getName())
-                                                        .english(ftsv.getEnglish())
-                                                        .build())
-                                                .type(ftsv.getType())
-                                                .source(SourceResponse.builder()
-                                                        .name(NameResponse.builder()
-                                                                .name(ftsv.getBookName())
-                                                                .label(ftsv.getBookAcronym())
-                                                                .english(ftsv.getBookEnglishName())
-                                                                .build())
-                                                        .group(NameResponse.builder()
-                                                                .name(ftsv.getBookType().getGroup())
-                                                                .label(ftsv.getBookType().getLabel())
-                                                                .build())
-                                                        .page(ftsv.getPage())
-                                                        .build())
-                                                .build()).collect(Collectors.toList()))
-                                .total(results.size())
+                .map(this::getFullTextSearchViewResponse)
+                .orElseGet(this::getEmptyResponse);
+    }
+
+    private FullTextSearchViewResponse getFullTextSearchViewResponse(Collection<FullTextSearchView> results) {
+        if (results.isEmpty()) {
+            return getEmptyResponse();
+        }
+
+        Map<FullTextSearchViewType, Integer> typeCount = new HashMap<>();
+
+        List<FullTextSearchViewDto> filtered = results.parallelStream()
+                .filter(ftsv -> counterFilter(ftsv, typeCount))
+                .map(this::getConvertedResult)
+                .collect(Collectors.toList());
+
+        return FullTextSearchViewResponse.builder()
+                .items(filtered)
+                .filtered(filtered.size())
+                .total(results.size())
+                .build();
+    }
+
+    private boolean counterFilter(FullTextSearchView item, Map<FullTextSearchViewType, Integer> typeCount) {
+        FullTextSearchViewType type = item.getType();
+        int count = typeCount.getOrDefault(type, 0);
+        if (count < maxItemsPerGroup) {
+            typeCount.put(type, count + 1);
+            return true;
+        }
+        return false;
+    }
+
+    private FullTextSearchViewDto getConvertedResult(FullTextSearchView ftsv) {
+        return FullTextSearchViewDto.builder()
+                .url(ftsv.getUrl())
+                .name(NameResponse.builder()
+                        .name(ftsv.getName())
+                        .english(ftsv.getEnglish())
+                        .build())
+                .type(ftsv.getType())
+                .source(SourceResponse.builder()
+                        .name(NameResponse.builder()
+                                .name(ftsv.getBookName())
+                                .label(ftsv.getBookAcronym())
+                                .english(ftsv.getBookEnglishName())
                                 .build())
-                .orElseGet(FullTextSearchViewResponse::new);
+                        .group(NameResponse.builder()
+                                .name(ftsv.getBookType().getGroup())
+                                .label(ftsv.getBookType().getLabel())
+                                .build())
+                        .page(ftsv.getPage())
+                        .build())
+                .build();
+    }
+
+    private FullTextSearchViewResponse getEmptyResponse() {
+        return FullTextSearchViewResponse.builder()
+                .items(Collections.emptyList())
+                .filtered(0)
+                .total(0)
+                .build();
     }
 
 }
