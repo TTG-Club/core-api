a26123df37a142e45f75bfdd46d906d31e6b43e1
ylp <yrlvdplsh@yandex.ru>
2025-08-11 18:20:14 +0300

    Фикс багов редактирования

--- src/main/java/club/ttg/dnd5/domain/spell/rest/mapper/SpellAffiliationMapper.java
+++ src/main/java/club/ttg/dnd5/domain/spell/rest/mapper/SpellAffiliationMapper.java
@@ -5,8 +5,11 @@ import club.ttg.dnd5.domain.common.model.NamedEntity;
 import club.ttg.dnd5.domain.spell.model.Spell;
 import club.ttg.dnd5.domain.spell.rest.dto.SpellAffiliationDto;
 import club.ttg.dnd5.domain.spell.rest.dto.SpellAffiliationResponse;
+import club.ttg.dnd5.domain.spell.rest.dto.create.CreateAffiliationRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface SpellAffiliationMapper {
@@ -14,4 +17,12 @@ public interface SpellAffiliationMapper {
 
     @Mapping(target = "species", source = "speciesAffiliation")
     SpellAffiliationResponse toSpellAffiliationResponse(Spell spell);
+
+    default CreateAffiliationRequest toCreateAffiliationRequest(Spell spell) {
+        return CreateAffiliationRequest.builder()
+                .species(spell.getSpeciesAffiliation().stream()
+                        .map(NamedEntity::getUrl)
+                        .collect(Collectors.toList()))
+                .build();
+    }
 }
