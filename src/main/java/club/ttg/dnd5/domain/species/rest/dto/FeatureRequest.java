cc22248775465192b3fa4304e02ebdab1bf8f193
yrlvdplsh <92540081+yrlvdplsh@users.noreply.github.com>
2025-08-12 20:19:08 +0300

    Feature/editor bugfix (#153)
    * Фикс багов редактирования
    
    * Фикс багов редактирования
    
    * Фикс багов редактирования
    
    * Фикс багов редактирования
    
    * Фикс багов редактирования
    
    * Фикс багов редактирования

--- src/main/java/club/ttg/dnd5/domain/species/rest/dto/FeatureRequest.java
+++ src/main/java/club/ttg/dnd5/domain/species/rest/dto/FeatureRequest.java
@@ -2,6 +2,8 @@ package club.ttg.dnd5.domain.species.rest.dto;
 
 import club.ttg.dnd5.domain.common.rest.dto.NameRequest;
 import club.ttg.dnd5.dto.base.SourceResponse;
+import club.ttg.dnd5.dto.base.deserializer.MarkupDescriptionDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +12,7 @@ import lombok.Setter;
 @Setter
 public class FeatureRequest {
     private NameRequest name;
+    @JsonDeserialize(using = MarkupDescriptionDeserializer.class)
     private String description;
     @Schema(description = "источник", requiredMode = Schema.RequiredMode.REQUIRED)
     private SourceResponse source = new SourceResponse();
