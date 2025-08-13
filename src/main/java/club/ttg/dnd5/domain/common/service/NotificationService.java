e57a9af53434f308a14f949961fb6d90932a7196
Vitalii Ungurean <vitalii@analytics.company>
2025-08-13 16:02:48 +0300

    feat: Нотификации

---
+++ src/main/java/club/ttg/dnd5/domain/common/service/NotificationService.java
@@ -0,0 +1,11 @@
+package club.ttg.dnd5.domain.common.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    public String getNotification() {
+        return null;
+    }
+}
