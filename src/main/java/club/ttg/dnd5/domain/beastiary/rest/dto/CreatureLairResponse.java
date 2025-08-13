e6ae85ec8257b7384af504a4f2d411dbfff2be14
Vitalii Ungurean <vitalii@analytics.company>
2025-08-13 16:32:17 +0300

    bugfix: Фикс ответа для Логова

--- src/main/java/club/ttg/dnd5/domain/beastiary/rest/dto/CreatureLairResponse.java
+++ src/main/java/club/ttg/dnd5/domain/beastiary/rest/dto/CreatureLairResponse.java
@@ -1,12 +1,14 @@
 package club.ttg.dnd5.domain.beastiary.rest.dto;
 
 import club.ttg.dnd5.dto.base.serializer.MarkupDescriptionSerializer;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Collection;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Setter
 public class CreatureLairResponse {
