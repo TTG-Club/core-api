75f3fe71dc6178d24e20e4d408825b53f4049b19
Vitalii Ungurean <vitalii@analytics.company>
2025-08-19 14:44:39 +0300

    fix: Исправление справочников

--- src/main/java/club/ttg/dnd5/domain/common/dictionary/CreatureType.java
+++ src/main/java/club/ttg/dnd5/domain/common/dictionary/CreatureType.java
@@ -24,7 +24,7 @@ public enum CreatureType {
 	PLANT("Растение"),
 	UNDEAD("Нежить"),
 	SWARM_OF_TINY_UNDEAD("Рой крошечной нежити"),
-	SWARM_OF_TINY_MONSTROSITIES("рой крошечных монстров"),
+	SWARM_OF_TINY_MONSTROSITIES("Рой крошечных монстров"),
 	SWARM_OF_TINY_BEASTS("Рой крошечных зверей"),
 	SWARM_OF_SMALL_FIENDS("Рой маленьких исчадий"),
 	SWARM_OF_MEDIUM_FIENDS("Рой средних исчадий"),
