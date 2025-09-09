0eda974885b40b6f9c20d9f8ac6cb970ccf601fa
Vitalii Ungurean <vitalii@analytics.company>
2025-09-09 14:21:23 +0300

    feat: фильтры

--- src/main/java/club/ttg/dnd5/domain/background/rest/dto/filter/AbilityFilterGroup.java
+++ src/main/java/club/ttg/dnd5/domain/background/rest/dto/filter/AbilityFilterGroup.java
@@ -5,53 +5,87 @@ import club.ttg.dnd5.dto.base.filters.AbstractFilterGroup;
 import club.ttg.dnd5.dto.base.filters.AbstractFilterItem;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.StringPath;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Getter
 @Setter
-public class AbilityFilterGroup extends AbstractFilterGroup<Ability, AbilityFilterGroup.AbilityFilterItem> {
-
-    private static final StringPath PATH = Expressions.stringPath("abilities");
-
-    public AbilityFilterGroup(List<AbilityFilterItem> filters) {
+public class AbilityFilterGroup extends AbstractFilterGroup<Ability, AbilityFilterGroup.AbilityFilterItem>
+{
+    public AbilityFilterGroup(List<AbilityFilterItem> filters)
+    {
         super(filters);
     }
 
     @Override
-    public BooleanExpression getQuery() {
-        if (isSingular()) {
+    public BooleanExpression getQuery()
+    {
+        if (isSingular())
+        {
             return TRUE_EXPRESSION;
         }
-        Set<Ability> positiveValues = getPositive();
-        BooleanExpression result = CollectionUtils.isEmpty(positiveValues) ? TRUE_EXPRESSION : PATH.in(positiveValues.stream().map(Ability::toString).collect(Collectors.toSet()));
-        Set<Ability> negativeValues = getNegative();
-        return result.and(CollectionUtils.isEmpty(negativeValues) ? (TRUE_EXPRESSION) : PATH.notIn(negativeValues.stream().map(Ability::toString).collect(Collectors.toSet())));
 
+        BooleanExpression result = TRUE_EXPRESSION;
+
+        Set<Ability> positive = getPositive();
+        if (CollectionUtils.isNotEmpty(positive))
+        {
+            String jsonArray = toJsonArray(positive);
+            result = result.and(Expressions.booleanTemplate(
+                    "abilities @> cast({0} as jsonb)", jsonArray
+            ));
+        }
+
+        Set<Ability> negative = getNegative();
+        if (CollectionUtils.isNotEmpty(negative))
+        {
+            for (Ability ability : negative)
+            {
+                String single = toJsonArray(List.of(ability));
+                result = result.and(Expressions.booleanTemplate(
+                        "not (abilities @> cast({0} as jsonb))", single
+                ));
+            }
+        }
+
+        return result;
     }
 
     @Override
-    public String getName() {
+    public String getName()
+    {
         return "Характеристики";
     }
 
-    public static AbilityFilterGroup getDefault() {
+    public static AbilityFilterGroup getDefault()
+    {
         return new AbilityFilterGroup(
                 Arrays.stream(Ability.values())
                         .map(AbilityFilterItem::new)
-                        .collect(Collectors.toList()));
+                        .collect(Collectors.toList())
+        );
     }
 
-    public static class AbilityFilterItem extends AbstractFilterItem<Ability> {
-        public AbilityFilterItem(Ability value) {
+    public static class AbilityFilterItem extends AbstractFilterItem<Ability>
+    {
+        public AbilityFilterItem(Ability value)
+        {
             super(value.getName(), value, null);
         }
+    }
+
+    private static String toJsonArray(Collection<Ability> values)
+    {
+        return values.stream()
+                .map(Ability::toString)
+                .map(s -> "\"" + s + "\"")
+                .collect(Collectors.joining(",", "[", "]"));
     }
 }
