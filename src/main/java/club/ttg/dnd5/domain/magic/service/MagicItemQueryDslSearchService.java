c6d5a5fae6aa84b5f395e5d45be2f3801ebf7262
Vitalii Ungurean <vitalii@analytics.company>
2025-09-10 09:52:46 +0300

    bugfix: исправление сортировки маг предметов

--- src/main/java/club/ttg/dnd5/domain/magic/service/MagicItemQueryDslSearchService.java
+++ src/main/java/club/ttg/dnd5/domain/magic/service/MagicItemQueryDslSearchService.java
@@ -3,7 +3,10 @@ package club.ttg.dnd5.domain.magic.service;
 import club.ttg.dnd5.domain.filter.service.AbstractQueryDslSearchService;
 import club.ttg.dnd5.domain.magic.model.MagicItem;
 import club.ttg.dnd5.domain.magic.model.QMagicItem;
+import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +20,28 @@ public class MagicItemQueryDslSearchService extends AbstractQueryDslSearchServic
 
     @Override
     protected OrderSpecifier<?>[] getOrder() {
-        return new OrderSpecifier[]{
-                MAGIC_ITEM.rarity.asc(),
+        NumberExpression<Integer> rarityRank = Expressions.numberTemplate(
+                Integer.class,
+                """
+                        case {0} \
+                         when 'VARIES' then -1\
+                         when 'COMMON' then 0\
+                         when 'UNCOMMON' then 1\
+                         when 'RARE' then 2\
+                         when 'VERY_RARE' then 3\
+                         when 'LEGENDARY' then 4\
+                         when 'ARTIFACT' then 5\
+                         when 'UNKNOWN' then 6\
+                         else 99 end
+                     """,
+                MAGIC_ITEM.rarity
+        );
+
+        OrderSpecifier<Integer> rarityOrder =
+                new OrderSpecifier<>(Order.ASC, rarityRank);
+
+        return new OrderSpecifier<?>[]{
+                rarityOrder,
                 MAGIC_ITEM.name.asc()
         };
     }
