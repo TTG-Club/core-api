20d3639bd162fc97defa0492b3efb01635b068ea
svifty7 <svifty7@gmail.com>
2025-09-29 12:34:34 +0300

    fix: изменил адрес ручек у классов

--- src/main/java/club/ttg/dnd5/domain/character_class/rest/controller/ClassController.java
+++ src/main/java/club/ttg/dnd5/domain/character_class/rest/controller/ClassController.java
@@ -4,8 +4,6 @@ import club.ttg.dnd5.domain.character_class.rest.dto.ClassDetailedResponse;
 import club.ttg.dnd5.domain.character_class.rest.dto.ClassRequest;
 import club.ttg.dnd5.domain.character_class.rest.dto.ClassShortResponse;
 import club.ttg.dnd5.domain.character_class.service.ClassService;
-import club.ttg.dnd5.domain.spell.rest.dto.SpellDetailedResponse;
-import club.ttg.dnd5.domain.spell.rest.dto.create.SpellRequest;
 import club.ttg.dnd5.exception.EntityNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -15,21 +13,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/class")
+@RequestMapping("/api/v2/classes")
 @Tag(name = "API для Классов", description = "API для управления классами")
 public class ClassController {
     private final ClassService classService;
