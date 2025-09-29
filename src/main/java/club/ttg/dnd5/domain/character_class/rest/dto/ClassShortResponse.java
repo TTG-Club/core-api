1239812de582a7b166b9c318b69cfdfc1cbcc8ba
svifty7 <svifty7@gmail.com>
2025-09-29 12:02:23 +0300

    fix: правки названия полей классов

--- src/main/java/club/ttg/dnd5/domain/character_class/rest/dto/ClassShortResponse.java
+++ src/main/java/club/ttg/dnd5/domain/character_class/rest/dto/ClassShortResponse.java
@@ -1,23 +1,17 @@
 package club.ttg.dnd5.domain.character_class.rest.dto;
 
-import club.ttg.dnd5.domain.common.dictionary.Dice;
-import club.ttg.dnd5.domain.common.rest.dto.BaseResponse;
-import club.ttg.dnd5.domain.common.rest.dto.select.DiceOptionDto;
+import club.ttg.dnd5.domain.common.rest.dto.ShortResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class ClassShortResponse extends BaseResponse {
-    @Schema(description = "Кость хитов класса")
-    private DiceOptionDto hitDice;
-    @Schema(description = "Подклассы")
-    private List<ClassShortResponse> subclasses;
+public class ClassShortResponse extends ShortResponse {
+    @Schema(description = "Имеет ли подклассы")
+    private boolean hasSubclasses;
 }
