475791433dfa204d99cc5927bf4ceffae4606bfb
Vitalii Ungurean <vitalii@analytics.company>
2025-10-07 11:15:05 +0300

    feat: добавление фильтра по классам в заклинаниях

--- src/main/java/club/ttg/dnd5/domain/spell/service/SpellFilterService.java
+++ src/main/java/club/ttg/dnd5/domain/spell/service/SpellFilterService.java
@@ -1,5 +1,6 @@
 package club.ttg.dnd5.domain.spell.service;
 
+import club.ttg.dnd5.domain.character_class.service.ClassService;
 import club.ttg.dnd5.domain.filter.model.FilterInfo;
 import club.ttg.dnd5.domain.filter.service.AbstractSavedFilterService;
 import club.ttg.dnd5.domain.spell.model.filter.SpellSavedFilter;
@@ -13,15 +14,21 @@ import java.util.List;
 @Service
 public class SpellFilterService extends AbstractSavedFilterService<SpellSavedFilter> {
     private static final String FILTER_VERSION = "1.0";
+    private final ClassService classService;
 
-    public SpellFilterService(SpellSavedFilterRepository spellSavedFilterRepository, UserService userService) {
+    public SpellFilterService(ClassService classService,
+            SpellSavedFilterRepository spellSavedFilterRepository,
+            UserService userService) {
         super(spellSavedFilterRepository, userService);
+        this.classService = classService;
     }
 
     @Override
     protected FilterInfo buildDefaultFilterInfo() {
         return new FilterInfo(List.of(
-                //SpellClassFilterGroup.getDefault(),
+                SpellClassFilterGroup.getDefault(
+                        classService.findAllClasses(null , null)
+                ),
                 SpellLevelFilterGroup.getDefault(),
                 SpellSchoolFilterGroup.getDefault(),
                 SpellOtherFilterGroup.getDefault(),
