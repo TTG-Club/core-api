df8974547e2384797c848c75ae4b92c442468e1d
Vitalii Ungurean <vitalii@analytics.company>
2025-10-07 12:38:29 +0300

    feat: добавление фильтра по подклассам в заклинаниях

--- src/main/java/club/ttg/dnd5/domain/spell/rest/dto/filter/SpellSubclassFilterGroup.java
+++ src/main/java/club/ttg/dnd5/domain/spell/rest/dto/filter/SpellSubclassFilterGroup.java
@@ -1,20 +1,30 @@
 package club.ttg.dnd5.domain.spell.rest.dto.filter;
 
+import club.ttg.dnd5.domain.character_class.rest.dto.ClassShortResponse;
+import club.ttg.dnd5.dto.base.filters.AbstractFilterGroup;
+import club.ttg.dnd5.dto.base.filters.AbstractFilterItem;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
- * Фильтр заклинаний по подклассам (ManyToMany: spell.classAffiliation).
- * Положительные значения: включает заклинания, у которых среди классов есть любой из указанных.
+ * Фильтр заклинаний по подклассам (ManyToMany: spell.subclassAffiliation).
+ * Положительные значения: включает заклинания, у которых среди подклассов есть любой из указанных.
  * Отрицательные значения: исключает заклинания, у которых есть любой из указанных.
  */
 @Getter
 @Setter
-public class SpellSubclassFilterGroup extends SpellClassFilterGroup
+public class SpellSubclassFilterGroup extends AbstractFilterGroup<String, SpellSubclassFilterGroup.SpellSubclassFilterItem>
 {
-    public SpellSubclassFilterGroup(final List<SpellClassFilterItem> filters) {
+    public SpellSubclassFilterGroup(List<SpellSubclassFilterItem> filters)
+    {
         super(filters);
     }
 
@@ -22,5 +32,63 @@ public class SpellSubclassFilterGroup extends SpellClassFilterGroup
     public String getName()
     {
         return "Подклассы";
+    }
+
+    @Override
+    public BooleanExpression getQuery() {
+        if (isSingular()) {
+            return TRUE_EXPRESSION;
+        }
+
+        final Set<String> positives = getPositive();
+        BooleanExpression result = CollectionUtils.isEmpty(positives)
+                ? TRUE_EXPRESSION
+                : Expressions.booleanTemplate(
+                "exists (" +
+                        "  select 1 " +
+                        "  from spell_subclass_affiliation sca " +
+                        "  where sca.spell_url = spell.url " +
+                        "    and sca.subclass_affiliation_url = any (cast({0} as text[]))" +
+                        ")",
+                Expressions.constant(positives.toArray(String[]::new))
+        );
+
+        final Set<String> negatives = getNegative();
+        return result.and(CollectionUtils.isEmpty(negatives)
+                ? TRUE_EXPRESSION
+                : Expressions.booleanTemplate(
+                "not exists (" +
+                        "  select 1 " +
+                        "  from spell_subclass_affiliation sca " +
+                        "  where sca.spell_url = spell.url " +
+                        "    and sca.subclass_affiliation_url = any (cast({0} as text[]))" +
+                        ")",
+                Expressions.constant(negatives.toArray(String[]::new))
+        ));
+    }
+
+    /**
+     * Построение дефолтного набора пунктов фильтра из списка классов.
+     */
+    public static SpellSubclassFilterGroup getDefault(final List<ClassShortResponse> allClasses)
+    {
+        final List<SpellSubclassFilterItem> items = allClasses
+                .stream()
+                .filter(Objects::nonNull)
+                .map(c -> new SpellSubclassFilterItem(
+                        c.getName().getName(), c.getUrl())
+                )
+                .collect(Collectors.toList());
+
+        return new SpellSubclassFilterGroup(items);
+    }
+
+
+    public static class SpellSubclassFilterItem extends AbstractFilterItem<String>
+    {
+        public SpellSubclassFilterItem(String label, String value)
+        {
+            super(label, value, null);
+        }
     }
 }
