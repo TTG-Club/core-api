68b03367f7fc5355cbf4217618c816f7ef7fae0f
Vitalii Ungurean <vitalii@analytics.company>
2025-10-14 11:50:50 +0300

    feat: Предметы (рефакторинг)

--- src/main/java/club/ttg/dnd5/domain/item/rest/dto/filter/ItemTypeFilterGroup.java
+++ src/main/java/club/ttg/dnd5/domain/item/rest/dto/filter/ItemTypeFilterGroup.java
@@ -5,7 +5,7 @@ import club.ttg.dnd5.dto.base.filters.AbstractFilterGroup;
 import club.ttg.dnd5.dto.base.filters.AbstractFilterItem;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.StringPath;
+import com.querydsl.core.types.dsl.SimplePath;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.collections4.CollectionUtils;
@@ -18,42 +18,77 @@ import java.util.stream.Collectors;
 
 @Getter
 @Setter
-public class ItemTypeFilterGroup extends AbstractFilterGroup<ItemType, ItemTypeFilterGroup.FilterItem> {
+public class ItemTypeFilterGroup extends AbstractFilterGroup<ItemType, ItemTypeFilterGroup.FilterItem>
+{
+    /** jsonb-колонка в таблице item */
+    private static final SimplePath<Object> PATH = Expressions.path(Object.class, "item_types");
 
-    private static final StringPath PATH = Expressions.stringPath("item_type");
-
-    public ItemTypeFilterGroup(List<FilterItem> filters) {
+    public ItemTypeFilterGroup(List<FilterItem> filters)
+    {
         super(filters);
     }
 
     @Override
-    public BooleanExpression getQuery() {
-        if (isSingular()) {
+    public BooleanExpression getQuery()
+    {
+        if (isSingular())
+        {
             return TRUE_EXPRESSION;
         }
-        Set<ItemType> positiveValues = getPositive();
-        BooleanExpression result = CollectionUtils.isEmpty(positiveValues) ? TRUE_EXPRESSION : PATH.in(positiveValues.stream().map(ItemType::toString).collect(Collectors.toSet()));
-        Set<ItemType> negativeValues = getNegative();
-        return result.and(CollectionUtils.isEmpty(negativeValues) ? (TRUE_EXPRESSION) : PATH.notIn(negativeValues.stream().map(ItemType::toString).collect(Collectors.toSet())));
 
+        final Set<ItemType> positiveValues = getPositive();
+        final Set<ItemType> negativeValues = getNegative();
+
+        BooleanExpression result = TRUE_EXPRESSION;
+
+        // Есть хотя бы один из позитивных типов: item_types ?| array['ARMOR','WEAPON',...]
+        if (CollectionUtils.isNotEmpty(positiveValues))
+        {
+            result = result.and(
+                    Expressions.booleanTemplate("{0} ?| array[" + toSqlTextArray(positiveValues) + "]", PATH)
+            );
+        }
+
+        // Нет ни одного из негативных типов: NOT (item_types ?| array['ARMOR','WEAPON',...])
+        if (CollectionUtils.isNotEmpty(negativeValues))
+        {
+            result = result.and(
+                    Expressions.booleanTemplate("NOT ({0} ?| array[" + toSqlTextArray(negativeValues) + "])", PATH)
+            );
+        }
+
+        return result;
     }
 
     @Override
-    public String getName() {
+    public String getName()
+    {
         return "Категория";
     }
 
-    public static ItemTypeFilterGroup getDefault() {
+    public static ItemTypeFilterGroup getDefault()
+    {
         return new ItemTypeFilterGroup(
                 Arrays.stream(ItemType.values())
                         .map(FilterItem::new)
                         .sorted(Comparator.comparing(FilterItem::getName))
-                        .collect(Collectors.toList()));
+                        .collect(Collectors.toList())
+        );
     }
 
-    public static class FilterItem extends AbstractFilterItem<ItemType> {
-        public FilterItem(ItemType value) {
+    public static class FilterItem extends AbstractFilterItem<ItemType>
+    {
+        public FilterItem(ItemType value)
+        {
             super(value.getName(), value, null);
         }
+    }
+
+    private static String toSqlTextArray(Set<ItemType> values)
+    {
+        return values.stream()
+                .map(ItemType::name)
+                .map(s -> "'" + s + "'")
+                .collect(Collectors.joining(","));
     }
 }
