30dc9b5a790efbbfb341deb7d84823a21c3bd28c
Vitalii Ungurean <vitalii@analytics.company>
2025-10-17 16:48:00 +0300

    bugfix: Убрал warning при запуске

--- src/main/java/club/ttg/dnd5/domain/species/model/SpeciesFeature.java
+++ src/main/java/club/ttg/dnd5/domain/species/model/SpeciesFeature.java
@@ -1,14 +1,16 @@
 package club.ttg.dnd5.domain.species.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class SpeciesFeature {
     private String url;
     private String name;
