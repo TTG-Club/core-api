f955fbce5e876472571e55c0a38897ab604b42ba
Vitalii Ungurean <vitalii@analytics.company>
2025-11-11 13:34:22 +0200

    feat: Источники (справочник типов)

--- src/main/java/club/ttg/dnd5/domain/source/model/SourceType.java
+++ src/main/java/club/ttg/dnd5/domain/source/model/SourceType.java
@@ -11,7 +11,7 @@ public enum SourceType {
     OFFICIAL("Базовые", "Официальные источники", "Basic"),
     SETTING("Сеттинги", "Официальные источники", "Basic"),
     MODULE("Приключения",  "Официальные источники", "Basic"),
-    TEST("Unearthed Arcana", "Тестовый материал", "UA"),
+    TEST("Неизведанная аркана", "Тестовый материал", "UA"),
     THIRD_PARTY("3rd party",  "Контент от третьих лиц", "3rd"),
     CUSTOM("Homebrew", "Самоделка", "HB");
 
