5b786247cbc95017fad7ab45e0ac4c1066a2a203
Vitalii Ungurean <vitalii@analytics.company>
2025-11-11 13:15:07 +0200

    bugfix: Источники

--- src/main/java/club/ttg/dnd5/domain/source/rest/mapper/SourceMapper.java
+++ src/main/java/club/ttg/dnd5/domain/source/rest/mapper/SourceMapper.java
@@ -8,6 +8,10 @@ import club.ttg.dnd5.dto.base.mapping.BaseMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Mapper(componentModel = "spring", uses = {
         BaseMapping.class
@@ -25,6 +29,7 @@ public interface SourceMapper {
     @Mapping(source = "english", target = "source.name.english")
     @Mapping(source = "acronym", target = "source.name.label")
     @Mapping(source = "type.name", target = "type")
+    @Mapping(source = "published", target = "published", qualifiedByName = "toPublishedString")
     SourceDetailResponse toDetail(Source source);
 
     @BaseMapping.BaseEntityNameMapping
@@ -36,4 +41,9 @@ public interface SourceMapper {
     @BaseMapping.BaseShortResponseNameMapping
     @Mapping(source = "acronym", target = "name.label")
     SourceRequest toRequest(Source source);
+
+    @Named("toPublishedString")
+    default String toPublishedString(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 }
