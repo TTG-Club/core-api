74b221078aa826b786a11791612a32d7571e3298
Vitalii Ungurean <vitalii@analytics.company>
2025-12-10 12:55:23 +0200

    feat: Сортировка по рейтингу в дорожной карте

--- src/main/java/club/ttg/dnd5/domain/roadmap/service/RoadmapService.java
+++ src/main/java/club/ttg/dnd5/domain/roadmap/service/RoadmapService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +33,7 @@ public class RoadmapService {
         return roadmaps.stream()
                 .map(roadmapMapper::toResponse)
                 .peek(r -> r.setRate(ratingService.getRating("ROADMAP", r.getUrl())))
+                .sorted(Comparator.comparingLong((RoadmapResponse r) -> r.getRate().getTotal()).reversed())
                 .collect(Collectors.toList());
     }
 
