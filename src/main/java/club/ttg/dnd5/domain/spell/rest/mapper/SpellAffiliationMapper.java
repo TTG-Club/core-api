aceb1db0446066cd59d23b89025faf9bbe77245f
Vitaliy Unguryan <gm.magistr@gmail.com>
2026-01-14 17:56:22 +0200

    hotfix: Добавление акронима в классах, подклассах, видах акронима источника (#222)

--- src/main/java/club/ttg/dnd5/domain/spell/rest/mapper/SpellAffiliationMapper.java
+++ src/main/java/club/ttg/dnd5/domain/spell/rest/mapper/SpellAffiliationMapper.java
@@ -1,7 +1,9 @@
 package club.ttg.dnd5.domain.spell.rest.mapper;
 
 
+import club.ttg.dnd5.domain.character_class.model.CharacterClass;
 import club.ttg.dnd5.domain.common.model.NamedEntity;
+import club.ttg.dnd5.domain.species.model.Species;
 import club.ttg.dnd5.domain.spell.model.Spell;
 import club.ttg.dnd5.domain.spell.rest.dto.SpellAffiliationDto;
 import club.ttg.dnd5.domain.spell.rest.dto.SpellAffiliationResponse;
@@ -13,7 +15,35 @@ import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface SpellAffiliationMapper {
-    SpellAffiliationDto toSpellAffiliationDto(NamedEntity entity);
+    default SpellAffiliationDto toSpellAffiliationDto(NamedEntity entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        SpellAffiliationDto dto = new SpellAffiliationDto();
+        dto.setUrl(entity.getUrl());
+
+        String name = entity.getName();
+        String sourceAcronym = null;
+
+        if (entity instanceof CharacterClass characterClass) {
+            if (characterClass.getSource() != null && characterClass.getSource().getAcronym() != null) {
+                sourceAcronym = "[" + characterClass.getSource().getAcronym() + "]";
+            }
+        } else if (entity instanceof Species species) {
+            if (species.getSource() != null && species.getSource().getAcronym() != null) {
+                sourceAcronym = "[" + species.getSource().getAcronym() + "]";
+            }
+        }
+
+        if (sourceAcronym != null) {
+            dto.setName(name + " " + sourceAcronym);
+        } else {
+            dto.setName(name);
+        }
+
+        return dto;
+    }
 
     @Mapping(target = "classes", source = "classAffiliation")
     @Mapping(target = "subclasses", source = "subclassAffiliation")
