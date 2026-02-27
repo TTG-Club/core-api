c22384741fc21d60c3ed6aad7bd986dcef9f3b63
Vitalii Ungurean <vitalii@analytics.company>
2026-02-27 16:34:49 +0200

    fix: Исправление источников

--- src/main/java/club/ttg/dnd5/domain/source/rest/mapper/SourceMapper.java
+++ src/main/java/club/ttg/dnd5/domain/source/rest/mapper/SourceMapper.java
@@ -3,15 +3,11 @@ package club.ttg.dnd5.domain.source.rest.mapper;
 import club.ttg.dnd5.domain.source.model.Source;
 import club.ttg.dnd5.domain.source.rest.dto.SourceDetailResponse;
 import club.ttg.dnd5.domain.source.rest.dto.SourceRequest;
-import club.ttg.dnd5.domain.common.rest.dto.ShortResponse;
+import club.ttg.dnd5.domain.source.rest.dto.SourceShortResponse;
 import club.ttg.dnd5.dto.base.mapping.BaseMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @Mapper(componentModel = "spring", uses = {
         BaseMapping.class
@@ -25,7 +21,7 @@ public interface SourceMapper {
     @Mapping(source = "name", target = "source.name.name")
     @Mapping(source = "english", target = "source.name.english")
     @Mapping(source = "acronym", target = "source.name.label")
-    ShortResponse toShort(Source source);
+    SourceShortResponse toShort(Source source);
 
     @BaseMapping.BaseShortResponseNameMapping
     @Mapping(target = "source.name", ignore = true)
@@ -47,8 +43,4 @@ public interface SourceMapper {
     @Mapping(source = "acronym", target = "name.label")
     SourceRequest toRequest(Source source);
 
-    @Named("toPublishedString")
-    default String toPublishedString(LocalDate date) {
-        return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
 }
