d1802832d32d3d38e387a8773c08427df8c66fb1
Peterko <peter.ko@mail.ru>
2026-04-03 21:48:51 +0300

    fix(notification): убрана обертка поля text в JSON-массив
    - Удалены аннотации @JsonSerialize(MarkupDescriptionSerializer) из NotificationDetailResponse
    - Удалены аннотации @JsonDeserialize(MarkupDescriptionDeserializer) из NotificationRequest
    
    Эти сериализаторы предназначались для описаний сущностей и оборачивали приходящую строку текста в формат массива. Из-за этого на фронтенде нотификации отображались с лишними скобками и кавычками. Теперь текст нотификации передается, сохраняется и отдается как простая строка.

--- src/main/java/club/ttg/dnd5/domain/common/rest/dto/notification/NotificationDetailResponse.java
+++ src/main/java/club/ttg/dnd5/domain/common/rest/dto/notification/NotificationDetailResponse.java
@@ -1,8 +1,6 @@
 package club.ttg.dnd5.domain.common.rest.dto.notification;
 
 import club.ttg.dnd5.domain.common.model.notification.NotificationType;
-import club.ttg.dnd5.dto.base.serializer.MarkupDescriptionSerializer;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +13,6 @@ public class NotificationDetailResponse {
     private NotificationType type;
     private String typeName;
     private String personaId;
-    @JsonSerialize(using = MarkupDescriptionSerializer.class)
     private String text;
     private Long view;
     private LocalDateTime after;
@@ -24,3 +21,4 @@ public class NotificationDetailResponse {
     private String username;
     private LocalDateTime createdAt;
 }
+
--- src/main/java/club/ttg/dnd5/domain/common/rest/dto/notification/NotificationRequest.java
+++ src/main/java/club/ttg/dnd5/domain/common/rest/dto/notification/NotificationRequest.java
@@ -1,8 +1,6 @@
 package club.ttg.dnd5.domain.common.rest.dto.notification;
 
 import club.ttg.dnd5.domain.common.model.notification.NotificationType;
-import club.ttg.dnd5.dto.base.deserializer.MarkupDescriptionDeserializer;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,10 +10,10 @@ public class NotificationRequest {
     private String id;
     private NotificationType type;
     private String personaId;
-    @JsonDeserialize(using = MarkupDescriptionDeserializer.class)
     private String text;
     private String before;
     private String after;
     private boolean disabled;
     private Long view;
 }
+
