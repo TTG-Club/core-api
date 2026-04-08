2b11b9fe38328064f7e51c895bd49b030d8967b1
Vitalii Ungurean <gm.magistr@gmail.com>
2026-04-08 12:54:59 +0300

    fix: Ускорение работы toDetail в заклинаниях для affiliations

--- src/main/java/club/ttg/dnd5/domain/spell/rest/mapper/SpellMapper.java
+++ src/main/java/club/ttg/dnd5/domain/spell/rest/mapper/SpellMapper.java
@@ -1,46 +1,56 @@
 package club.ttg.dnd5.domain.spell.rest.mapper;
 
-import club.ttg.dnd5.domain.source.model.Source;
 import club.ttg.dnd5.domain.character_class.model.CharacterClass;
+import club.ttg.dnd5.domain.common.model.NamedEntity;
+import club.ttg.dnd5.domain.source.model.Source;
 import club.ttg.dnd5.domain.species.model.Species;
 import club.ttg.dnd5.domain.spell.model.MaterialComponent;
 import club.ttg.dnd5.domain.spell.model.Spell;
 import club.ttg.dnd5.domain.spell.model.SpellCastingTime;
 import club.ttg.dnd5.domain.spell.model.SpellComponents;
-import club.ttg.dnd5.domain.spell.model.SpellDistance;
 import club.ttg.dnd5.domain.spell.model.SpellDuration;
 import club.ttg.dnd5.domain.spell.model.enums.CastingUnit;
+import club.ttg.dnd5.domain.spell.rest.dto.SpellAffiliationDto;
+import club.ttg.dnd5.domain.spell.rest.dto.SpellAffiliationResponse;
 import club.ttg.dnd5.domain.spell.rest.dto.SpellDetailedResponse;
 import club.ttg.dnd5.domain.spell.rest.dto.SpellShortResponse;
 import club.ttg.dnd5.domain.spell.rest.dto.create.SpellRequest;
 import club.ttg.dnd5.dto.base.mapping.BaseMapping;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.util.StringUtils;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import org.mapstruct.ReportingPolicy;
-
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = {SpellComponentsMapper.class, SpellAffiliationMapper.class, BaseMapping.class}, componentModel = "spring")
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        uses = {SpellComponentsMapper.class, BaseMapping.class},
+        componentModel = "spring"
+)
 public interface SpellMapper
 {
     @ToEntityMapping
-    Spell toEntity(SpellRequest request,
-                   Source source,
-                   java.util.List<CharacterClass> classes,
-                   java.util.List<CharacterClass> subclasses,
-                   java.util.List<Species> species,
-                   java.util.List<Species> lineages);
+    Spell toEntity(
+            SpellRequest request,
+            Source source,
+            List<CharacterClass> classes,
+            List<CharacterClass> subclasses,
+            List<Species> species,
+            List<Species> lineages
+    );
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "name", source = "request.name.name")
@@ -60,16 +70,119 @@ public interface SpellMapper
     @BaseMapping.BaseShortResponseNameMapping
     @Mapping(target = "school", source = "school.school.name", qualifiedByName = "capitalize")
     @Mapping(target = "additionalType", source = "school.additionalType")
-    @Mapping(target = "castingTime", source = ".", qualifiedByName = "castingTimeToString")
-    @Mapping(target = "duration", source = ".", qualifiedByName = "durationToString")
-    @Mapping(target = "range", source = ".", qualifiedByName = "distanceToString")
+    @Mapping(target = "castingTime", ignore = true)
+    @Mapping(target = "duration", ignore = true)
+    @Mapping(target = "range", ignore = true)
     @Mapping(target = "source", source = "source.acronym")
-    @Mapping(target = "affiliation", source = ".")
+    @Mapping(target = "affiliation", ignore = true)
     SpellDetailedResponse toDetail(Spell spell);
+
+    @AfterMapping
+    default void fillDetailFields(Spell spell, @MappingTarget SpellDetailedResponse target)
+    {
+        target.setCastingTime(joinSpellValues(spell.getCastingTime()));
+        target.setDuration(joinSpellValues(spell.getDuration()));
+        target.setRange(joinSpellValues(spell.getRange()));
+        target.setAffiliation(buildAffiliation(spell));
+    }
+
+    default SpellAffiliationResponse buildAffiliation(Spell spell)
+    {
+        SpellAffiliationResponse response = new SpellAffiliationResponse();
+        response.setClasses(mapAffiliations(spell.getClassAffiliation()));
+        response.setSubclasses(mapAffiliations(spell.getSubclassAffiliation()));
+        response.setSpecies(mapAffiliations(spell.getSpeciesAffiliation()));
+        response.setLineages(mapAffiliations(spell.getLineagesAffiliation()));
+        return response;
+    }
+
+    default List<SpellAffiliationDto> mapAffiliations(List<? extends NamedEntity> entities)
+    {
+        if (entities == null || entities.isEmpty())
+        {
+            return List.of();
+        }
+
+        List<SpellAffiliationDto> result = new ArrayList<>(entities.size());
+        for (NamedEntity entity : entities)
+        {
+            SpellAffiliationDto dto = toAffiliationDto(entity);
+            if (dto != null)
+            {
+                result.add(dto);
+            }
+        }
+        return result;
+    }
+
+    default SpellAffiliationDto toAffiliationDto(NamedEntity entity)
+    {
+        if (entity == null)
+        {
+            return null;
+        }
+
+        SpellAffiliationDto dto = new SpellAffiliationDto();
+        dto.setUrl(entity.getUrl());
+
+        String name = entity.getName();
+        String sourceAcronym = null;
+
+        if (entity instanceof CharacterClass characterClass)
+        {
+            if (characterClass.getSource() != null && StringUtils.hasText(characterClass.getSource().getAcronym()))
+            {
+                sourceAcronym = characterClass.getSource().getAcronym();
+            }
+        }
+        else if (entity instanceof Species species)
+        {
+            if (species.getSource() != null && StringUtils.hasText(species.getSource().getAcronym()))
+            {
+                sourceAcronym = species.getSource().getAcronym();
+            }
+        }
+
+        if (sourceAcronym != null)
+        {
+            dto.setSource(sourceAcronym);
+            dto.setName(name + " [" + sourceAcronym + "]");
+        }
+        else
+        {
+            dto.setName(name);
+        }
+
+        return dto;
+    }
+
+    default String joinSpellValues(List<?> values)
+    {
+        if (values == null || values.isEmpty())
+        {
+            return null;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < values.size(); i++)
+        {
+            if (i > 0)
+            {
+                builder.append(" или ");
+            }
+            builder.append(values.get(i));
+        }
+        return builder.toString();
+    }
 
     @Named("isConcentration")
     default Boolean isConcentration(Collection<SpellDuration> durations)
     {
+        if (durations == null || durations.isEmpty())
+        {
+            return false;
+        }
+
         return durations.stream()
                 .map(SpellDuration::getConcentration)
                 .anyMatch(Predicate.isEqual(true));
@@ -78,33 +191,14 @@ public interface SpellMapper
     @Named("isRitual")
     default Boolean isRitual(Collection<SpellCastingTime> castingTimes)
     {
+        if (castingTimes == null || castingTimes.isEmpty())
+        {
+            return false;
+        }
+
         return castingTimes.stream()
                 .map(SpellCastingTime::getUnit)
                 .anyMatch(u -> CastingUnit.RITUAL == u);
-    }
-
-    @Named("castingTimeToString")
-    default String castingTimeToString(Spell spell)
-    {
-        return spell.getCastingTime().stream()
-                .map(SpellCastingTime::toString)
-                .collect(Collectors.joining(" или "));
-    }
-
-    @Named("durationToString")
-    default String durationToString(Spell spell)
-    {
-        return spell.getDuration().stream()
-                .map(SpellDuration::toString)
-                .collect(Collectors.joining(" или "));
-    }
-
-    @Named("distanceToString")
-    default String distanceToString(Spell spell)
-    {
-        return spell.getRange().stream()
-                .map(SpellDistance::toString)
-                .collect(Collectors.joining(" или "));
     }
 
     @BaseMapping.BaseRequestNameMapping
@@ -166,6 +260,7 @@ public interface SpellMapper
         {
             return null;
         }
+
         return values.stream()
                 .filter(StringUtils::hasText)
                 .collect(Collectors.joining("; "));
