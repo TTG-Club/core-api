47230b56517c9d7fab60b993256433dbfac48bb5
Vitaliy Unguryan <gm.magistr@gmail.com>
2026-07-27 20:55:13 +0300

    feat(species): добавить уровни врождённых заклинаний (#312)
    Использовать существующие связи заклинаний с видами и происхождениями, хранить требуемый уровень и возвращать данные редактору и листу персонажа.
    
    Co-authored-by: dev <dev@ttg.club>

---
+++ src/main/java/club/ttg/dnd5/domain/species/repository/SpeciesInnateSpellView.java
@@ -0,0 +1,8 @@
+package club.ttg.dnd5.domain.species.repository;
+
+public interface SpeciesInnateSpellView
+{
+    String getSpellUrl();
+
+    Integer getRequiredLevel();
+}
--- src/main/java/club/ttg/dnd5/domain/species/repository/SpeciesRepository.java
+++ src/main/java/club/ttg/dnd5/domain/species/repository/SpeciesRepository.java
@@ -5,6 +5,7 @@ import club.ttg.dnd5.domain.vttg.repository.VttgEntityRef;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +16,44 @@ import java.util.List;
 @Repository
 public interface SpeciesRepository extends JpaRepository<Species, String> {
     Collection<Species> findByParent(Species parent);
+
+    @Query(value = """
+            select spell_url as "spellUrl", required_level as "requiredLevel"
+            from spell_species_affiliation
+            where species_affiliation_url = :speciesUrl
+            union all
+            select spell_url as "spellUrl", required_level as "requiredLevel"
+            from spell_lineages_affiliation
+            where lineages_affiliation_url = :speciesUrl
+            order by "requiredLevel", "spellUrl"
+            """, nativeQuery = true)
+    List<SpeciesInnateSpellView> findInnateSpells(@Param("speciesUrl") String speciesUrl);
+
+    @Modifying
+    @Query(value = "delete from spell_species_affiliation where species_affiliation_url = :speciesUrl", nativeQuery = true)
+    void deleteSpeciesInnateSpells(@Param("speciesUrl") String speciesUrl);
+
+    @Modifying
+    @Query(value = "delete from spell_lineages_affiliation where lineages_affiliation_url = :speciesUrl", nativeQuery = true)
+    void deleteLineageInnateSpells(@Param("speciesUrl") String speciesUrl);
+
+    @Modifying
+    @Query(value = """
+            insert into spell_species_affiliation (spell_url, species_affiliation_url, required_level)
+            values (:spellUrl, :speciesUrl, :requiredLevel)
+            """, nativeQuery = true)
+    void addSpeciesInnateSpell(@Param("speciesUrl") String speciesUrl,
+                               @Param("spellUrl") String spellUrl,
+                               @Param("requiredLevel") Integer requiredLevel);
+
+    @Modifying
+    @Query(value = """
+            insert into spell_lineages_affiliation (spell_url, lineages_affiliation_url, required_level)
+            values (:spellUrl, :speciesUrl, :requiredLevel)
+            """, nativeQuery = true)
+    void addLineageInnateSpell(@Param("speciesUrl") String speciesUrl,
+                               @Param("spellUrl") String spellUrl,
+                               @Param("requiredLevel") Integer requiredLevel);
 
     @Query(value = """
         select s from Species s
